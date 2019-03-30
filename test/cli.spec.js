ebe2fe4d03967b0dd56966677c5af48de7de437a
Owen Buckley <owenbuckley13@gmail.com>
2019-03-30 18:58:18 -0400

    lint test files

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -3,17 +3,17 @@ const fs = require('fs-extra');
 const path = require('path');
 const glob = require('glob-promise');
 const TestSetup = require('./setup');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const CONFIG = {
-    pagesDir: path.join(__dirname, '../packages/cli/templates/'),
-    scratchDir: path.join(__dirname, '..', './.greenwood/'),
-    templatesDir: path.join(__dirname, '../packages/cli/templates/'),
-    publicDir: path.join(__dirname, '..', './public'),
-    testApp: path.join(__dirname, 'fixtures', 'mock-app', 'src'),
-    usrSrc: path.join(__dirname, '..', 'src'),
-    usrTemplate: path.join(__dirname, '..', 'src', 'templates')
+  pagesDir: path.join(__dirname, '../packages/cli/templates/'),
+  scratchDir: path.join(__dirname, '..', './.greenwood/'),
+  templatesDir: path.join(__dirname, '../packages/cli/templates/'),
+  publicDir: path.join(__dirname, '..', './public'),
+  testApp: path.join(__dirname, 'fixtures', 'mock-app', 'src'),
+  usrSrc: path.join(__dirname, '..', 'src'),
+  usrTemplate: path.join(__dirname, '..', 'src', 'templates')
 };
 
 const whenSerialized = (head, par) => {
@@ -74,38 +74,60 @@ describe('after building greenwood', () => {
 
 describe('after building greenwood', () => {
 
-    before(async () => {
-        setup = new TestSetup();
-        await setup.run(['./packages/cli/index.js', '']);
-    });  
+  before(async () => {
+    setup = new TestSetup();
+    await setup.run(['./packages/cli/index.js', '']);
+  });  
 
-    it('should create a new public directory', () => {
-        expect(fs.existsSync(CONFIG.publicDir)).to.be.true;
+  it('should create a new public directory', () => {
+    expect(fs.existsSync(CONFIG.publicDir)).to.be.true;
+  });
+
+  describe('within the public folder', () => {
+    it('should contain an index.html file', () => {
+      expect(fs.existsSync(path.join(CONFIG.publicDir, './index.html'))).to.be.true;
     });
-
-    describe('within the public folder', () => {
-        it('should contain an index.html file', () => {
-            expect(fs.existsSync(path.join(CONFIG.publicDir, './index.html'))).to.be.true;
-        });
-        it('should contain a js bundle file', async () => {
-            expect(await glob.promise(path.join(CONFIG.publicDir, './index.*.bundle.js'))).to.have.lengthOf(1);
-        });
-        it('should contain a hello directory', () => {
-            expect(fs.existsSync(path.join(CONFIG.publicDir, './hello'))).to.be.true;
-        });
-        it('should contain an index.html file within the hello world directory', () => {
-            expect(fs.existsSync(path.join(CONFIG.publicDir, './hello', './index.html'))).to.be.true;
-        });
+    it('should contain a js bundle file', async () => {
+      expect(await glob.promise(path.join(CONFIG.publicDir, './index.*.bundle.js'))).to.have.lengthOf(1);
     });
+    it('should contain a hello directory', () => {
+      expect(fs.existsSync(path.join(CONFIG.publicDir, './hello'))).to.be.true;
+    });
+    it('should contain an index.html file within the hello world directory', () => {
+      expect(fs.existsSync(path.join(CONFIG.publicDir, './hello', './index.html'))).to.be.true;
+    });
+  });
 
+<<<<<<< HEAD
     describe('using default greenwood template', () => {
         whenSerialized('Hello World', 'This is an example page built by Greenwood.  Make your own in src/pages!');
         after(async() => {
             await fs.remove(CONFIG.publicDir);
             await fs.remove(CONFIG.scratchDir);
         });
+=======
+  describe('when serialized', async () => {
+    let dom;
+
+    before(async() => {
+      dom = await JSDOM.fromFile(path.resolve(__dirname, '..', './public/hello/index.html'));
     });
 
+    it('should display the hello world heading', async () => {
+      let heading = dom.window.document.querySelector('h3.wc-md-hello').textContent;
+
+      expect(heading).to.equal('Hello World');
+    });
+
+    it('should display the hello world text', async () => {
+      let paragraph = dom.window.document.querySelector('p.wc-md-hello').textContent;
+
+      expect(paragraph).to.equal('This is an example page built by Greenwood.  Make your own in src/pages!');
+>>>>>>> lint test files
+    });
+  });
+
+<<<<<<< HEAD
     describe('with a correct user templates directory', () => {
         before(async() => {
             // copy test app
@@ -119,4 +141,10 @@ describe('after building greenwood', () => {
             await fs.remove(CONFIG.scratchDir);
         })
     }); 
+=======
+  after(async() => {
+    await fs.remove(CONFIG.publicDir);
+    await fs.remove(CONFIG.scratchDir);
+  });
+>>>>>>> lint test files
 });
