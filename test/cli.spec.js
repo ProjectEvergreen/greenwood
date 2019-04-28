b066cac3cd9b932d9e63e2fe3bc7befa09e7fdb9
HutchGrant <h.g.utchinson@gmail.com>
2019-04-28 11:43:04 -0400

    test: removing test function, readding tests

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -7,16 +7,126 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 let CONTEXT;
 
-const testUserWorkspace = async (withoutTemplate, template) => {
+describe('building greenwood with default context (no user workspace)', () => {
+
+  before(async () => {
+    setup = new TestSetup();
+    CONTEXT = await setup.init();
+
+    await setup.run(['./packages/cli/index.js', 'build']);
+  });
+
+  it('should create a public directory', () => {
+    expect(fs.existsSync(CONTEXT.publicDir)).to.be.true;
+  });
+
+  describe('public directory output', () => {  
+    it('should output a single index.html file (home page)', () => {
+      expect(fs.existsSync(path.join(CONTEXT.publicDir, './index.html'))).to.be.true;
+    });
+  
+    it('should output one JS bundle file', async () => {
+      expect(await glob.promise(path.join(CONTEXT.publicDir, './index.*.bundle.js'))).to.have.lengthOf(1);
+    });
+  
+    it('should create a default hello page directory', () => {
+      expect(fs.existsSync(path.join(CONTEXT.publicDir, './hello'))).to.be.true;
+    });
+
+    describe('default generated hello page directory', () => {
+      const defaultHeading = 'Hello World';
+      const defaultBody = 'This is an example page built by Greenwood.  Make your own in src/pages!';
+      let dom;
+
+      beforeEach(async() => {
+        dom = await JSDOM.fromFile(path.resolve(CONTEXT.publicDir, 'hello/index.html'));
+      });
+
+      it('should output an index.html file within the default hello page directory', () => {
+        expect(fs.existsSync(path.join(CONTEXT.publicDir, './hello', './index.html'))).to.be.true;
+      });
+
+      it('should have the expected heading text within the hello example page in the hello directory', async() => {
+        const heading = dom.window.document.querySelector('h3.wc-md-hello').textContent;
+    
+        expect(heading).to.equal(defaultHeading);
+      });
+    
+      it('should have the expected paragraph text within the hello example page in the hello directory', async() => {
+        let paragraph = dom.window.document.querySelector('p.wc-md-hello').textContent;
+    
+        expect(paragraph).to.equal(defaultBody);
+      });
+    });
+  });
+
+  after(async() => {
+    await fs.remove(CONTEXT.publicDir);
+    await fs.remove(CONTEXT.scratchDir);
+  });
+
+});
+
+describe('building greenwood with a user workspace w/custom nested pages directories', () => {
+
   before(async() => {
     setup = new TestSetup();
     CONTEXT = await setup.init();
     // copy test app
     await fs.copy(CONTEXT.testApp, CONTEXT.userSrc);
     await setup.run(['./packages/cli/index.js', 'build']);
-    if (withoutTemplate) {
-      await fs.removeSync(path.join(CONTEXT.userSrc, 'templates', `${template}-template.js`));
-    }
+    
+    blogPageHtmlPath = path.join(CONTEXT.publicDir, 'blog', '20190326', 'index.html'); 
+  });
+
+  it('should output one JS bundle', async() => {
+    expect(await glob.promise(path.join(CONTEXT.publicDir, './**/index.*.bundle.js'))).to.have.lengthOf(1);
+  });
+  
+  it('should contain a nested blog page directory', () => {
+    expect(fs.existsSync(path.join(CONTEXT.publicDir, 'blog', '20190326'))).to.be.true;
+  });
+  
+  describe('nested generated blog page directory', () => {
+    const defaultHeading = 'Blog Page';
+    const defaultBody = 'This is the blog page built by Greenwood.';
+    let dom;
+    
+    beforeEach(async() => {
+      dom = await JSDOM.fromFile(blogPageHtmlPath);
+    });
+
+    it('should contain a nested blog page with an index html file', () => {
+      expect(fs.existsSync(blogPageHtmlPath)).to.be.true;
+    });
+
+    it('should have the expected heading text within the blog page in the blog directory', async() => {
+      const heading = dom.window.document.querySelector('h3.wc-md-blog').textContent;
+  
+      expect(heading).to.equal(defaultHeading);
+    });
+  
+    it('should have the expected paragraph text within the blog page in the blog directory', async() => {
+      let paragraph = dom.window.document.querySelector('p.wc-md-blog').textContent;
+  
+      expect(paragraph).to.equal(defaultBody);
+    });
+  });
+  after(async() => {
+    await fs.remove(CONTEXT.userSrc);
+    await fs.remove(CONTEXT.publicDir);
+    await fs.remove(CONTEXT.scratchDir);
+  });
+});
+
+describe('building greenwood with user workspace that doesn\'t contain app template', () => {
+  before(async() => {
+    setup = new TestSetup();
+    CONTEXT = await setup.init();
+    // copy test app
+    await fs.copy(CONTEXT.testApp, CONTEXT.userSrc);
+    await setup.run(['./packages/cli/index.js', 'build']);
+    await fs.removeSync(path.join(CONTEXT.userSrc, 'templates', 'app-template.js'));
     
     blogPageHtmlPath = path.join(CONTEXT.publicDir, 'blog', '20190326', 'index.html'); 
   });
@@ -99,16 +209,17 @@ const testUserWorkspace = async (withoutTemplate, template) => {
     await fs.remove(CONTEXT.publicDir);
     await fs.remove(CONTEXT.scratchDir);
   });
-
-};
-
-describe('building greenwood with default context (no user workspace)', () => {
-
-  before(async () => {
+});
+describe('building greenwood with user workspace that doesn\'t contain page template', () => {
+  before(async() => {
     setup = new TestSetup();
     CONTEXT = await setup.init();
-
+    // copy test app
+    await fs.copy(CONTEXT.testApp, CONTEXT.userSrc);
     await setup.run(['./packages/cli/index.js', 'build']);
+    await fs.removeSync(path.join(CONTEXT.userSrc, 'templates', 'page-template.js'));
+    
+    blogPageHtmlPath = path.join(CONTEXT.publicDir, 'blog', '20190326', 'index.html'); 
   });
 
   it('should create a public directory', () => {
@@ -119,18 +230,18 @@ describe('building greenwood with default context (no user workspace)', () => {
     it('should output a single index.html file (home page)', () => {
       expect(fs.existsSync(path.join(CONTEXT.publicDir, './index.html'))).to.be.true;
     });
-  
-    it('should output one JS bundle file', async () => {
-      expect(await glob.promise(path.join(CONTEXT.publicDir, './index.*.bundle.js'))).to.have.lengthOf(1);
+
+    it('should output one JS bundle', async() => {
+      expect(await glob.promise(path.join(CONTEXT.publicDir, './**/index.*.bundle.js'))).to.have.lengthOf(1);
     });
-  
+
     it('should create a default hello page directory', () => {
       expect(fs.existsSync(path.join(CONTEXT.publicDir, './hello'))).to.be.true;
     });
 
     describe('default generated hello page directory', () => {
-      const defaultHeading = 'Hello World';
-      const defaultBody = 'This is an example page built by Greenwood.  Make your own in src/pages!';
+      const defaultHeading = 'Test App';
+      const defaultBody = 'This is a test app using a custom user template!';
       let dom;
 
       beforeEach(async() => {
@@ -154,25 +265,49 @@ describe('building greenwood with default context (no user workspace)', () => {
       });
     });
   });
+  
+  it('should contain a nested blog page directory', () => {
+    expect(fs.existsSync(path.join(CONTEXT.publicDir, 'blog', '20190326'))).to.be.true;
+  });
+  
+  describe('nested generated blog page directory', () => {
+    const defaultHeading = 'Blog Page';
+    const defaultBody = 'This is the blog page built by Greenwood.';
+    let dom;
+    
+    beforeEach(async() => {
+      dom = await JSDOM.fromFile(blogPageHtmlPath);
+    });
 
+    it('should contain a nested blog page with an index html file', () => {
+      expect(fs.existsSync(blogPageHtmlPath)).to.be.true;
+    });
+
+    it('should have the expected heading text within the blog page in the blog directory', async() => {
+      const heading = dom.window.document.querySelector('h3.wc-md-blog').textContent;
+  
+      expect(heading).to.equal(defaultHeading);
+    });
+  
+    it('should have the expected paragraph text within the blog page in the blog directory', async() => {
+      let paragraph = dom.window.document.querySelector('p.wc-md-blog').textContent;
+  
+      expect(paragraph).to.equal(defaultBody);
+    });
+  });
   after(async() => {
+    await fs.remove(CONTEXT.userSrc);
     await fs.remove(CONTEXT.publicDir);
     await fs.remove(CONTEXT.scratchDir);
   });
-
 });
 
-describe('building greenwood with a user workspace w/custom nested pages directories', () => {
-  testUserWorkspace();
-});
+// TODO - https://github.com/ProjectEvergreen/greenwood/issues/32
+// describe('building greenwood with a user workspace w/custom app-template override', () => {
 
-describe('building greenwood with user workspace that doesn\'t contain app template', () => {
-  const withoutTemplate = true;
+// });
 
-  testUserWorkspace(withoutTemplate, 'app');
-});
-describe('building greenwood with user workspace that doesn\'t contain page template', () => {
-  const withoutTemplate = true;
+// TODO - https://github.com/ProjectEvergreen/greenwood/issues/30
+// describe('building greenwood with a user workspace w/custom page-template override', () => {
 
-  testUserWorkspace(withoutTemplate, 'page');
-});
+// });
