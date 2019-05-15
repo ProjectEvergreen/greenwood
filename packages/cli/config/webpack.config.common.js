98ccc8676c38d6291f0f2af8ddffde63c3441876
Grant Hutchinson <h.g.utchinson@gmail.com>
2019-05-14 20:25:37 -0400

    Global markdown CSS (#78)
    * fix: add wc-markdown-loader v0.1.1 and updating webpack config
    
    * fix: wc-markdown-loader v0.1.2
    
    * test: adding css tests via puppeteer
    
    * fix: puppeteer args
    
    * fix: removing default markdown style
    
    * fix: removing puppeteer

--- packages/cli/config/webpack.config.common.js
+++ packages/cli/config/webpack.config.common.js
@@ -56,7 +56,11 @@ module.exports = (config, context) => {
         }
       }, {
         test: /\.md$/,
-        loader: 'wc-markdown-loader'
+        loader: 'wc-markdown-loader',
+        options: {
+          defaultStyle: false,
+          shadowRoot: false
+        }
       }, {
         test: /\.css$/,
         loaders: [
