d035157198ee9c65c6217f6dd992efeae1812274
HutchGrant <h.g.utchinson@gmail.com>
2019-05-29 21:40:03 -0400

    fix: revert master config

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  // workspace: path.join(__dirname, 'www'),
+  workspace: path.join(__dirname, 'www'),
   title: 'Greenwood',
-  publicPath: '/assets/',
   meta: [
     { property: 'og:site', content: 'greenwood' },
     { name: 'twitter:site', content: '@PrjEvergreen' }
