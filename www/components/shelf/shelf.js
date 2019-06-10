d8fa54d3164e270e8e85ea71c6627826b6026d71
HutchGrant <h.g.utchinson@gmail.com>
2019-06-10 19:41:10 -0400

    fix: adding hash scroll component, amending shelf to have property for list, adding placeholder pages

--- www/components/shelf/shelf.js
+++ www/components/shelf/shelf.js
@@ -1,5 +1,9 @@
 import { html, LitElement } from 'lit-element';
 import css from './shelf.css';
+import { applyMiddleware, createStore, compose as origCompose, combineReducers } from 'redux';
+import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
+import thunk from 'redux-thunk';
+import { navigate } from 'lit-redux-router';
 
 const chevronRt = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
 <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505
@@ -11,47 +15,66 @@ const chevronDwn = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448
 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373
 9.372-24.569 9.372-33.942 0z"/></svg>`;
 
-let shelfList;
+// eslint-disable-next-line no-underscore-dangle
+const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;
+// eslint-disable-next-line
+const store = createStore(
+  (state, action) => state, // eslint-disable-line
+  compose(lazyReducerEnhancer(combineReducers), applyMiddleware(thunk)));
 
 class shelf extends LitElement {
+
+  static get properties() {
+    return {
+      shelfList: {
+        type: Array
+      }
+    };
+  }
 
   constructor() {
     super();
     this.selectedIndex = '';
     this.selectedSubIndex = '';
+
   }
 
   connectedCallback() {
     super.connectedCallback();
-
-    // based on path, display selected list
-    const path = window.location.pathname;
-
-    if (path.substring(0, 5) === '/docs') {
-      shelfList = require('./documentation-list.json');
-    } else {
-      shelfList = require('./getting-started-list.json');
-    }
-
     this.collapseAll();
+    this.expandRoute(window.location.pathname);
   }
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
+  goTo(path) {
+    location.hash = path;
+    store.dispatch(navigate(path));
+  }
+
+  expandRoute(path) {
+    // find list item containing current window.location.pathname
+    let routeShelfListIndex = this.shelfList.findIndex(list => {
+      return list.path === path;
+    });
+
+    if (routeShelfListIndex > -1) {
+      this.shelfList[routeShelfListIndex].selected = true;
+      // force re-render
+      this.requestUpdate();
+    }
   }
 
   collapseAll() {
-    for (let i = 0; i < shelfList.length; i = i + 1) {
-      shelfList[i].selected = false;
+    for (let i = 0; i < this.shelfList.length; i = i + 1) {
+      this.shelfList[i].selected = false;
     }
   }
 
   toggleSelectedItem() {
-    let selectedShelfListIndex = shelfList.findIndex(list => {
+    let selectedShelfListIndex = this.shelfList.findIndex(list => {
       return list.index === this.selected;
     });
 
-    shelfList[selectedShelfListIndex].selected = !shelfList[selectedShelfListIndex].selected;
+    this.shelfList[selectedShelfListIndex].selected = !this.shelfList[selectedShelfListIndex].selected;
   }
 
   setSelectedItem(evt) {
@@ -85,20 +108,20 @@ class shelf extends LitElement {
         <ul>
           ${list.items.map(item => {
             return html`
-              <li id="index_${item.index}" class="${list.selected ? '' : 'hidden'}">${item.name}</li>
+              <li id="index_${item.index}" class="${list.selected ? '' : 'hidden'}"><a @click=${()=> this.goTo(`#${item.id}`)}">${item.name}</a></li>
             `;
           })}
         </ul>`;
     }
     /* eslint-enable */
 
-    return shelfList.map((list) => {
+    return this.shelfList.map((list) => {
       let id = `index_${list.index}`;
       let chevron = list.selected === true ? chevronDwn : chevronRt;
 
       return html`
         <li class="list-wrap">
-          <a href="#" @click="${this.handleClick}"><h2 id="${id}">${list.name} <span>${chevron}</span></h2></a>
+          <a href="${list.path}" @click="${this.handleClick}"><h2 id="${id}">${list.name} <span>${chevron}</span></h2></a>
           <hr>
           ${renderListItems(list)}
         </li>
--- www/templates/page-template.js
+++ www/templates/page-template.js
@@ -5,12 +5,32 @@ import '../components/header/header';
 import '../components/footer/footer';
 import '@evergreen-wc/eve-container';
 import '../components/shelf/shelf';
+import '../components/scroll/scroll';
 
 MDIMPORT;
 METAIMPORT;
 METADATA;
 
+let shelfList = [];
+
 class PageTemplate extends LitElement {
+
+  constructor() {
+    super();
+    this.setupShelf();
+  }
+
+  setupShelf() {
+    // based on path, display selected list
+    const path = window.location.pathname;
+
+    if (path.substring(0, 5) === '/docs') {
+      shelfList = require('../components/shelf/documentation-list.json');
+    } else {
+      shelfList = require('../components/shelf/getting-started-list.json');
+    }
+  }
+
   render() {
     return html`
       <style>
@@ -22,14 +42,16 @@ class PageTemplate extends LitElement {
         <eve-header></eve-header>
         <div class='content-wrapper'>
           <div class="sidebar">
-            <eve-shelf></eve-shelf>
+            <eve-shelf .shelfList="${shelfList}"></eve-shelf>
           </div>
           <div class="content">
             <eve-container fluid>
-              <entry></entry>
+              <eve-scroll>
+                <entry></entry>
+              </eve-scroll>
             </eve-container>
           </div>
-          </div>
+        </div>
         <eve-footer></eve-footer>
       </div>
     `;
