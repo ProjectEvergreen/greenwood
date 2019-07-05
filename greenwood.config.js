9e9630b084a9b9c276540ad5b8ff59ddc0b08ec2
HutchGrant <h.g.utchinson@gmail.com>
2019-07-05 14:02:05 -0400

    fix: reverting greenwood config

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  // workspace: path.join(__dirname, 'www'),
+  workspace: path.join(__dirname, 'www'),
   title: 'Greenwood',
   meta: [
     { property: 'og:site', content: 'greenwood' },
