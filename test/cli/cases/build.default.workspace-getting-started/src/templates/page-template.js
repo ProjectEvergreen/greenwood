03b58e5af8c5341cd8973f4d2167babc5b4aad30
Owen Buckley <owenbuckley13@gmail.com>
2019-08-05 21:18:11 -0400

    getting started full test case

---
+++ test/cli/cases/build.default.workspace-getting-started/src/templates/page-template.js
@@ -0,0 +1,34 @@
+import { html, LitElement } from 'lit-element';
+import '../components/footer';
+import '../components/header';
+import '../styles/theme.css';
+
+MDIMPORT;
+
+class PageTemplate extends LitElement {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      
+      <style>
+        section {
+          margin: 0 auto;
+          width: 70%;
+        }
+      </style>
+    
+
+      <div>
+        <app-header></app-header>
+        <entry></entry>
+        <app-footer></app-footer>
+      </div>
+    `;
+  }
+}
+
+customElements.define('page-template', PageTemplate);
