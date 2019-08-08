67d0db4e1ec366b9875bbe0d1cfbef9782637937
Owen Buckley <owenbuckley13@gmail.com>
2019-08-07 20:57:55 -0400

    eslint for www

--- www/templates/page-template.js
+++ www/templates/page-template.js
@@ -1,11 +1,10 @@
 import { html, LitElement } from 'lit-element';
-import Prism from 'prismjs';
+import Prism from 'prismjs'; // eslint-disable-line no-unused-vars
 import '../components/header/header';
 import '../components/footer/footer';
 import '@evergreen-wc/eve-container';
 import '../components/shelf/shelf';
 import '../components/scroll/scroll';
-
 import pageCss from '../styles/page.css';
 import '../styles/theme.css';
 
