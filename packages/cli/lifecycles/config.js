88765d8e8039867ec235717924dfa7a404f6cd1b
Owen Buckley <owenbuckley13@gmail.com>
2019-08-16 19:20:36 -0400

    update message

--- packages/cli/lifecycles/config.js
+++ packages/cli/lifecycles/config.js
@@ -82,7 +82,7 @@ module.exports = readAndMergeConfig = async() => {
           if (devServer.host) {
             // eslint-disable-next-line max-depth
             if (url.parse(devServer.host).pathname === null) {
-              reject(`Error: greenwood.config.js devServer host type must be a valid url, including http://.  Passed value was: ${devServer.host}`);
+              reject(`Error: greenwood.config.js devServer host type must be a valid pathname.  Passed value was: ${devServer.host}`);
             } else {
               customConfig.devServer.host = devServer.host;
               // console.log(`custom host provided => ${customConfig.devServer.host}`);
