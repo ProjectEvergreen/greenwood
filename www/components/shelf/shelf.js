5ed5667a9d75c9639053cf8d1c88d40652762fbc
HutchGrant <h.g.utchinson@gmail.com>
2019-10-17 21:47:08 -0400

    fix: remove redux, fix getting-started menu

--- www/components/shelf/shelf.js
+++ www/components/shelf/shelf.js
@@ -1,9 +1,5 @@
 import { html, LitElement } from 'lit-element';
 import css from './shelf.css';
-import { applyMiddleware, createStore, compose as origCompose, combineReducers } from 'redux';
-import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
-import thunk from 'redux-thunk';
-import { navigate } from 'lit-redux-router';
 
 const chevronRt = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
 <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505
@@ -14,13 +10,6 @@ const chevronDwn = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448
 <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224
 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373
 9.372-24.569 9.372-33.942 0z"/></svg>`;
-
-// eslint-disable-next-line no-underscore-dangle
-const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;
-// eslint-disable-next-line
-const store = createStore(
-  (state, action) => state, // eslint-disable-line
-  compose(lazyReducerEnhancer(combineReducers), applyMiddleware(thunk)));
 
 class shelf extends LitElement {
 
@@ -47,7 +36,7 @@ class shelf extends LitElement {
 
   goTo(path) {
     location.hash = path;
-    store.dispatch(navigate(path));
+    window.history.pushState({}, '', path);
   }
 
   expandRoute(path) {
