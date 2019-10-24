c243c1185e946196bca7e1b4281f2f395cf35fff
Adam Holtzman <adam.holtzman@gmail.com>
2019-10-24 19:34:24 -0400

    ISSUE #124 - refactor chevron svgs by removing them from the shelf component into… (#243)
    * refactor cheveron svgs by removing them from the shelf component into a svg component for importing
    
    * add in missing semi-colon
    
    * bresking up is easy to do, make the chevron SVGs thier own components
    
    * rearrange files, remane folders for ease of setting up testing and keeping the clutter down

--- www/components/shelf/shelf.js
+++ www/components/shelf/shelf.js
@@ -1,15 +1,7 @@
 import { html, LitElement } from 'lit-element';
 import css from './shelf.css';
-
-const chevronRt = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-<path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505
-256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373
-24.568.001 33.941z"/></svg>`;
-
-const chevronDwn = html`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-<path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224
-284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373
-9.372-24.569 9.372-33.942 0z"/></svg>`;
+import chevronRt from '../icons/chevron-right/chevron-right';
+import chevronDwn from '../icons/chevron-down/chevron-down';
 
 class shelf extends LitElement {
 
