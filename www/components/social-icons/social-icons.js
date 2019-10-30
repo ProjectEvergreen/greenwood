89cad0011b233652d176c001abd227df55a876c6
Adam Holtzman <adam.holtzman@gmail.com>
2019-10-29 20:34:08 -0400

    add social links with icon componet to header (#249)
    * add social links with icone componet to header
    
    * clean up
    
    * stab at fixing the svgs so they render
    
    * change lit-element tag from html to svg and resize
    
    * change out the svg icons
    
    * clean up
    
    * fightig the good fight with the 200 line max
    
    * try the simple stuff first...
    
    * fix the poor octo-cat, maybe the twitter bird apears?
    
    * new twitter svg - fingers crossed this one works
    
    * fine tine the styling of the social bar & icons
    
    * clean up, add icon padding, center align nav text
    
    * indentation

---
+++ www/components/social-icons/social-icons.js
@@ -0,0 +1,24 @@
+import { html, LitElement } from 'lit-element';
+import css from './social-icons.css';
+import githubIcon from '../icons/github-icon';
+import twitterIcon from '../icons/twitter-icon';
+import slackIcon from '../icons/slack-icon';
+
+class SocialIcons extends LitElement {
+  render() {
+    return html`
+    <style>
+      ${css}
+    </style>
+        <a class="icons" href="https://github.com/ProjectEvergreen" target="_blank" rel=”noreferrer noopener” aria-label="open github page">${githubIcon}</a>
+        <a href="https://join.slack.com/t/thegreenhouseio/shared_invite/enQtMzcyMzE2Mjk1MjgwLTU5YmM1MDJiMTg0ODk4MjA4NzUwNWFmZmMxNDY5MTcwM2I0MjYxN2VhOTEwNDU2YWQwOWQzZmY1YzY4MWRlOGI"
+         class="icons slack-icon"
+         target="_blank"
+         rel=”noreferrer noopener”
+         aria-label="slack">${slackIcon}</a>
+        <a href="https://twitter.com/PrjEvergreen" target="_blank" rel=”noreferrer noopener” aria-label="open twitter page">${twitterIcon}</a>
+    `;
+  }
+}
+
+customElements.define('eve-socialicons', SocialIcons);
