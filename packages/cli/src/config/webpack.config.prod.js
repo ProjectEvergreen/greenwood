d2998c9687b8bf576abf29c0a9379ae0e39fa6a7
Owen Buckley <owenbuckley13@gmail.com>
2019-11-20 23:44:32 -0500

    linting

--- packages/cli/src/config/webpack.config.prod.js
+++ packages/cli/src/config/webpack.config.prod.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.config.common.js');
 
