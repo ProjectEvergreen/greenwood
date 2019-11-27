df169d57cfcd3424c3807df78553301e196db1ea
Owen Buckley <owenbuckley13@gmail.com>
2019-11-26 21:39:12 -0500

    fix github link and refactor social icons a little (#262)
    * fix github link and refactor social icons a little
    
    * formatting
    
    * variable naming

--- www/components/social-icons/social-icons.js
+++ www/components/social-icons/social-icons.js
@@ -6,19 +6,41 @@ import slackIcon from '../icons/slack-icon';
 
 class SocialIcons extends LitElement {
   render() {
+    const socialUrls = {
+      github: 'https://github.com/ProjectEvergreen/greenwood',
+      slack: 'https://join.slack.com/t/thegreenhouseio/shared_invite/enQtMzcyMzE2Mjk1MjgwLTU5YmM1MDJiMTg0ODk4MjA4NzUwNWFmZmMxNDY5MTcwM2I0MjYxN2VhOTEwNDU2YWQwOWQzZmY1YzY4MWRlOGI',
+      twitter: 'https://twitter.com/PrjEvergreen'
+    };
+
     return html`
-    <style>
-      ${css}
-    </style>
-        <a class="icons" href="https://github.com/ProjectEvergreen" target="_blank" rel=”noreferrer noopener” aria-label="open github page">${githubIcon}</a>
-        <a href="https://join.slack.com/t/thegreenhouseio/shared_invite/enQtMzcyMzE2Mjk1MjgwLTU5YmM1MDJiMTg0ODk4MjA4NzUwNWFmZmMxNDY5MTcwM2I0MjYxN2VhOTEwNDU2YWQwOWQzZmY1YzY4MWRlOGI"
-         class="icons slack-icon"
-         target="_blank"
-         rel=”noreferrer noopener”
-         aria-label="slack">${slackIcon}</a>
-        <a href="https://twitter.com/PrjEvergreen" target="_blank" rel=”noreferrer noopener” aria-label="open twitter page">${twitterIcon}</a>
+      <style>
+        ${css}
+      </style>
+
+      <a class="icons" 
+        href="${socialUrls.github}"
+        target="_blank" 
+        rel="noreferrer noopener" 
+        aria-label="open github page"
+        @onclick="getOutboundLink('${socialUrls.github}'); return false;" 
+      >${githubIcon}</a>
+      
+      <a href="${socialUrls.slack}"
+        class="icons slack-icon" 
+        target="_blank" 
+        rel="noreferrer noopener"
+        aria-label="slack"
+        @onclick="getOutboundLink('${socialUrls.slack}'); return false;" 
+      >${slackIcon}</a>
+      
+      <a href="${socialUrls.twitter}" 
+        target="_blank" 
+        rel="noreferrer noopener" 
+        aria-label="open twitter page"
+        @onclick="getOutboundLink('${socialUrls.twitter}'); return false;" 
+      >${twitterIcon}</a>
     `;
   }
 }
 
-customElements.define('eve-socialicons', SocialIcons);
+customElements.define('eve-social-icons', SocialIcons);
