03b8c977d285ab0fa3bc3bc35974148d90f4f413
Owen Buckley <owenbuckley13@gmail.com>
2020-01-25 17:28:19 -0500

    refactor browser runner init

--- packages/cli/src/lifecycles/serialize.js
+++ packages/cli/src/lifecycles/serialize.js
@@ -1,38 +1,39 @@
 const LocalWebServer = require('local-web-server');
 const BrowserRunner = require('../lib/browser');
 const fs = require('fs-extra');
-const localWebServer = new LocalWebServer();
 const path = require('path');
-const PORT = '8000';
-const polyfillPath = path.join(process.cwd(), 'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js');
-let polyfill = '';
-
-browserRunner = new BrowserRunner();
-browserRunner.init();
-
-const runBrowser = async (compilation) => {
-  try {
-    return Promise.all(compilation.graph.map(async({ route }) => {
-      const { publicDir } = compilation.context;
-
-      return await browserRunner.serialize(`http://127.0.0.1:${PORT}${route}`).then(async (content) => {
-        const target = path.join(publicDir, route);
-        const html = content
-          .replace(polyfill, '')
-          .replace('<script></script>', '');
-
-        await fs.mkdirs(target, { recursive: true });
-        await fs.writeFile(path.join(target, 'index.html'), html);
-      });
-    }));
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-    return false;
-  }
-};
 
 module.exports = serializeBuild = async (compilation) => {
+  const browserRunner = new BrowserRunner();
+  const localWebServer = new LocalWebServer();
+  const PORT = '8000';
+  const polyfillPath = path.join(process.cwd(), 'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js');
+  let polyfill = '';
+  
+  await browserRunner.init();
+
+  const runBrowser = async (compilation) => {
+    try {
+      return Promise.all(compilation.graph.map(async({ route }) => {
+        const { publicDir } = compilation.context;
+  
+        return await browserRunner.serialize(`http://127.0.0.1:${PORT}${route}`).then(async (content) => {
+          const target = path.join(publicDir, route);
+          const html = content
+            .replace(polyfill, '')
+            .replace('<script></script>', '');
+  
+          await fs.mkdirs(target, { recursive: true });
+          await fs.writeFile(path.join(target, 'index.html'), html);
+        });
+      }));
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      return false;
+    }
+  };
+  
   return new Promise(async (resolve, reject) => {
     try {
       polyfill = await fs.readFile(polyfillPath, 'utf8');
