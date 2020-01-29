f2ce77350de41e805a629e35b87328d93e376b0b
Owen Buckley <owenbuckley13@gmail.com>
2020-01-28 20:40:09 -0500

    testing refactoring for build matrix (#266)
    * testing refactoring for build matrix
    
    * refactor run usage
    
    * fix RUN command
    
    * fix RUN command
    
    * refactor run command
    
    * sudo
    
    * refactor lib install to script
    
    * fix script file path
    
    * fix script file path
    
    * oops, forgot file lol
    
    * sudo all the things
    
    * remove async
    
    * refactor browser runner init
    
    * refactor browser runner init

---
+++ .github/workflows/chromium-lib-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+sudo apt-get update \
+  && sudo apt-get install -yq libgconf-2-4 \
+  && sudo apt-get install -y wget --no-install-recommends \
+  && sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
+  && sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+  && sudo apt-get update \
+  && sudo apt-get install -y google-chrome-unstable --no-install-recommends \
+  && sudo rm -rf /var/lib/apt/lists/*
