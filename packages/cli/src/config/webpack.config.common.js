f5faf588d3f8032c8be9f608cb94ce886678d8c2
HutchGrant <h.g.utchinson@gmail.com>
2020-02-16 19:32:27 -0500

    fix: fix lint errors

--- packages/cli/src/config/webpack.config.common.js
+++ packages/cli/src/config/webpack.config.common.js
@@ -129,7 +129,7 @@ module.exports = ({ config, context }) => {
 
     optimization: {
       splitChunks: {
-        chunks: 'all',
+        chunks: 'all'
       }
     },
 
@@ -161,5 +161,5 @@ module.exports = ({ config, context }) => {
 
       ...customWebpackPlugins
     ]
-  }
-}
+  };
+};
