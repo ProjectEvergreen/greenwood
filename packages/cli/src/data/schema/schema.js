b9f56f641e470cfbf4f5b0278cc70577613b8209
Owen Buckley <owenbuckley13@gmail.com>
2020-03-03 23:31:10 -0500

    delete hello query example

--- packages/cli/src/data/schema/schema.js
+++ packages/cli/src/data/schema/schema.js
@@ -1,7 +1,6 @@
 const { makeExecutableSchema } = require('apollo-server-express');
 const { configTypeDefs, configResolvers } = require('./config');
 const { graphTypeDefs, graphResolvers } = require('./graph');
-const { helloTypeDefs, helloResolvers } = require('./hello');
 
 const mergedResolvers = Object.assign({}, {
   Query: {
