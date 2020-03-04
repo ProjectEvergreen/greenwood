4b481aabcc1b35d16213a2f06e8f3efb4829ba6d
Owen Buckley <owenbuckley13@gmail.com>
2020-03-03 21:59:01 -0500

    config def and resolvers

--- packages/cli/src/data/schema/schema.js
+++ packages/cli/src/data/schema/schema.js
@@ -1,6 +1,7 @@
 const { makeExecutableSchema } = require('apollo-server-express');
 const { configTypeDefs, configResolvers } = require('./config');
 const { graphTypeDefs, graphResolvers } = require('./graph');
+const { helloTypeDefs, helloResolvers } = require('./hello');
 
 const mergedResolvers = Object.assign({}, {
   Query: {
