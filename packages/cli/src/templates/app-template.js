2fccd6e2ea088b4a37cee0e7af33f361cfca64a5
Owen Buckley <owenbuckley13@gmail.com>
2020-03-07 19:04:57 -0500

    app template with config title from data

--- packages/cli/src/templates/app-template.js
+++ packages/cli/src/templates/app-template.js
@@ -3,6 +3,8 @@ import { connectRouter } from 'lit-redux-router';
 import { applyMiddleware, createStore, compose as origCompose, combineReducers } from 'redux';
 import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
 import thunk from 'redux-thunk';
+import client from '@greenwood/cli/data/client';
+import ConfigQuery from '@greenwood/cli/data/queries/config';
 
 // eslint-disable-next-line no-underscore-dangle
 const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;
@@ -17,6 +19,27 @@ import './list';
 connectRouter(store);
 
 class AppComponent extends LitElement {
+
+  async connectedCallback() {
+    super.connectedCallback();
+
+    const response = await client.query({
+      query: ConfigQuery
+    });
+    const { config } = response.data;
+
+    console.log('app template config', response.data.config);
+    this.setDocoumentTitle(config.title);
+  }
+
+  setDocoumentTitle(title) {
+    console.log('setDocoumentTitle', title);
+    const head = document.head;
+    const titleElement = head.getElementsByTagName('title')[0];
+
+    titleElement.innerHTML = title;
+  }
+
   render() {
     return html`
         MYROUTES
