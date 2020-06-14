a3d4e89743c64374d86b298cc80d2ab1bd82560b
HutchGrant <h.g.utchinson@gmail.com>
2020-06-14 13:48:08 -0400

    test: update test clarify config changes

--- packages/cli/test/cases/build.config.babel/babel.config.js
+++ packages/cli/test/cases/build.config.babel/babel.config.js
@@ -1,36 +1,20 @@
 module.exports = {
-  
-  // https://github.com/babel/babel/issues/9937#issuecomment-489352549
   sourceType: 'unambiguous',
-  
-  // https://github.com/babel/babel/issues/8731#issuecomment-426522500
   ignore: [/[\/\\]core-js/, /@babel[\/\\]runtime/],
-
-  // https://github.com/zloirock/core-js/blob/master/docs/2019-03-19-core-js-3-babel-and-a-look-into-the-future.md#babelpreset-env
   presets: [
     [
-      // https://babeljs.io/docs/en/babel-preset-env
       '@babel/preset-env',
       {
-        
-        // https://babeljs.io/docs/en/babel-preset-env#usebuiltins
         useBuiltIns: 'entry',
-        
-        // https://babeljs.io/docs/en/babel-preset-env#corejs
         corejs: { 
           version: 3,
           proposals: true
         },
-
-        // https://babeljs.io/docs/en/babel-preset-env#configpath
         configPath: __dirname
       }
     ]
   ],
 
-  // https://github.com/babel/babel/issues/8829#issuecomment-456524916
-  plugins: [
-
-  ]
-
+  // Here we've deliberately removed plugins so that it will compile but won't compile correctly which we can test for
+  plugins: []
 };
