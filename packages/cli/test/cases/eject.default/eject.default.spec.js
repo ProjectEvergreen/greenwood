dc1d2abc052d5a7ede93b51643d91c8cd48315a0
HutchGrant <h.g.utchinson@gmail.com>
2020-06-22 15:22:26 -0400

    test: check for specific file names

--- packages/cli/test/cases/eject.default/eject.default.spec.js
+++ packages/cli/test/cases/eject.default/eject.default.spec.js
@@ -40,6 +40,18 @@ describe('Eject Greenwood With: ', function() {
       expect(configFiles.length).to.equal(3);
     });
 
+    it('should output webpack common config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'webpack.config.common.js'))).to.be.true;
+    });
+
+    it('should output webpack develop config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'webpack.config.develop.js'))).to.be.true;
+    });
+
+    it('should output webpack prod config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'webpack.config.prod.js'))).to.be.true;
+    });
+
     after(function() {
       // remove files
       const configFiles = fs.readdirSync(__dirname);
@@ -72,6 +84,30 @@ describe('Eject Greenwood With: ', function() {
       * 'webpack.config.prod.js'
       */
       expect(configFiles.length).to.equal(6);
+    });
+
+    it('should output webpack common config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'webpack.config.common.js'))).to.be.true;
+    });
+
+    it('should output webpack develop config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'webpack.config.develop.js'))).to.be.true;
+    });
+
+    it('should output webpack prod config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'webpack.config.prod.js'))).to.be.true;
+    });
+
+    it('should output babel config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'babel.config.js'))).to.be.true;
+    });
+
+    it('should output postcss config', function() {
+      expect(fs.existsSync(path.join(__dirname, 'postcss.config.js'))).to.be.true;
+    });
+
+    it('should output browserslist', function() {
+      expect(fs.existsSync(path.join(__dirname, '.browserslistrc'))).to.be.true;
     });
 
     after(function() {
