32eb2acd7136b85c371457b8579b64f5ec907e5a
hutchgrant <grant@hutchdev.ca>
2020-07-11 00:42:16 -0400

    fix: lint issues

--- www/templates/app-template.js
+++ www/templates/app-template.js
@@ -8,7 +8,7 @@ import ConfigQuery from '@greenwood/cli/data/queries/config';
 import GraphQuery from '@greenwood/cli/data/queries/graph';
 import '../components/header/header';
 import '../components/footer/footer';
-LDIMPORT
+LDIMPORT;
 
 // eslint-disable-next-line no-underscore-dangle
 const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;
--- www/templates/loading.js
+++ www/templates/loading.js
@@ -1,19 +1,19 @@
 import { html, css, LitElement } from 'lit-element';
 
-class LoadingComponent extends LitElement {
-    static get styles() {
-        return css`
-            div {
-                height: calc(100vh - 70px - 40px);
-                width: 100vw;
-            }
-        `
-    }
-    render() {
-        return html`
-            <div></div>
-        `;
-    }
+class Loading extends LitElement {
+  static get styles() {
+    return css`
+      div {
+        height: 100vh;
+        width: 100vw;
+      }
+    `;
+  }
+  render() {
+    return html`
+      <div></div>
+    `;
+  }
 }
 
-customElements.define('eve-loading', LoadingComponent);
+customElements.define('eve-loading', Loading);
