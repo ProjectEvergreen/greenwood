e72ca70bf52197ebfc589f659647de7bc5c624e4
Owen Buckley <owenbuckley13@gmail.com>
2020-07-31 20:11:14 -0400

    add error test case for mode

---
+++ packages/cli/test/cases/build.config.error-mode/build.config.error-mode.spec.js
@@ -0,0 +1,44 @@
+/*
+ * Use Case
+ * Run Greenwood build command with a bad value for mode in a custom config.
+ *
+ * User Result
+ * Should throw an error.
+ *
+ * User Command
+ * greenwood build
+ *
+ * User Config
+ * {
+ *   mode: 'lorumipsum'
+ * }
+ *
+ * User Workspace
+ * Greenwood default
+ */
+const expect = require('chai').expect;
+const TestBed = require('../../../../../test/test-bed');
+
+describe.only('Build Greenwood With: ', function() {
+  let setup;
+
+  before(async function() {
+    setup = new TestBed(true);
+    await setup.setupTestBed(__dirname);
+  });
+
+  describe('Custom Configuration with a bad value for Mode', function() {
+    it('should throw an error that provided mode is not valid', async function() {
+      try {
+        await setup.runGreenwoodCommand('build');
+      } catch (err) {
+        expect(err).to.contain('Error: provided mode "loremipsum" is not supported.  Please use one of: strict, spa.');
+      }
+    });
+  });
+
+  after(function() {
+    setup.teardownTestBed();
+  });
+
+});
