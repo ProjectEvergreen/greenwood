aaa25a67de2717df2416dde3e87f45dff60120f5
Owen Buckley <owenbuckley13@gmail.com>
2020-09-17 13:08:56 -0400

    import CSS support

--- packages/cli/src/lifecycles/serve.js
+++ packages/cli/src/lifecycles/serve.js
@@ -208,10 +208,24 @@ app.use(async ctx => {
   }
 
   if (ctx.request.url.indexOf('.css') >= 0) {
+    const destHeader = ctx.request.header['sec-fetch-dest'];
     const cssPath = path.join(userWorkspace, ctx.request.url);
+    const css = await fsp.readFile(cssPath, 'utf-8');
 
-    ctx.set('Content-Type', 'text/css');
-    ctx.body = await fsp.readFile(cssPath, 'utf-8');
+    // <style> tag used
+    if (destHeader === 'style') {
+      ctx.set('Content-Type', 'text/css');
+      ctx.body = css;
+    } else if (destHeader === 'empty') {
+      // assume JS import being being used
+      ctx.set('Content-Type', 'text/javascript');
+      // TODO line breaks are bad for fetch, need to return CSS string all on one line
+      ctx.body = `
+        const css = "${css.replace(/\r?\n|\r/g, ' ')}";
+
+        export default css;
+      `;
+    }
   }
 
   if (ctx.request.url.indexOf('assets/') >= 0 && ctx.request.url.indexOf('.css') < 0) {
--- www/components/footer/footer.js
+++ www/components/footer/footer.js
@@ -1,15 +1,19 @@
-import { html, LitElement } from 'lit-element';
-// make sure version gets bumped first when building for release
+import { css, html, LitElement, unsafeCSS } from 'lit-element';
 import json from '../../package.json';
-// import footerCss from './footer.css';
+import footerCss from './footer.css';
 
 class FooterComponent extends LitElement {
+
+  static get styles() {
+    return css`
+      ${unsafeCSS(footerCss)}
+    `;
+  }
 
   render() {
     const { version } = json;
 
     return html`
-      <!-- TODO use static styles for footerCss -->
       <footer class="footer">
         <h4>
           <a href="/">Greenwood v${version}</a>
