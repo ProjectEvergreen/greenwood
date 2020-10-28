2b45ae43bdc75a3d928255c7f12e4f2b48840897
Owen Buckley <owenbuckley13@gmail.com>
2020-10-28 17:09:34 -0400

    example of using gallinago inline

--- packages/cli/test/cases/build.config.default/build.config.default.spec.js
+++ packages/cli/test/cases/build.config.default/build.config.default.spec.js
@@ -14,27 +14,37 @@
  * User Workspace
  * Greenwood default (src/)
  */
+const path = require('path');
 const runSmokeTest = require('../../../../../test/smoke-test');
-const TestBed = require('../../../../../test/test-bed');
+const Runner = require('../../../../../test/gallinago').Runner;
 
 describe('Build Greenwood With: ', function() {
-  const LABEL = 'Empty User Configuration and No Workspace';
-  let setup;
+  const LABEL = 'Empty Configuration and Default Workspace';
+  const cliPath = path.join(process.cwd(), 'packages/cli/src/index.js');
+  const outputPath = path.join(__dirname, 'output');
+  let runner;
 
   before(async function() {
-    setup = new TestBed();
-    this.context = await setup.setupTestBed(__dirname);
+    this.context = {
+      publicDir: path.join(outputPath, 'public')
+    };
+    runner = new Runner();
   });
 
   describe(LABEL, function() {
     before(async function() {
-      await setup.runGreenwoodCommand('build');
+      await runner.setup(outputPath, [{
+        source: path.join(process.cwd(), 'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js'),
+        destination: path.join(outputPath, 'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle.js')
+      }]);
+
+      await runner.runCommand(cliPath, 'build');
     });
 
     runSmokeTest(['public', 'index'], LABEL);
   });
 
   after(function() {
-    setup.teardownTestBed();
+    runner.teardown();
   });
 });
