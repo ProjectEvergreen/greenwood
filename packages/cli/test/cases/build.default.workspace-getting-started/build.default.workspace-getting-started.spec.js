0bd82774cf0ec578bffc6ffa24fcbf1fa6625ed4
Owen Buckley <owenbuckley13@gmail.com>
2020-12-10 11:27:36 -0500

    delete describe.only

--- packages/cli/test/cases/build.default.workspace-getting-started/build.default.workspace-getting-started.spec.js
+++ packages/cli/test/cases/build.default.workspace-getting-started/build.default.workspace-getting-started.spec.js
@@ -36,7 +36,7 @@ const { JSDOM } = require('jsdom');
 const path = require('path');
 const TestBed = require('../../../../../test/test-bed');
 
-describe.only('Build Greenwood With: ', function() {
+describe('Build Greenwood With: ', function() {
   const LABEL = 'Custom Workspace based on the Getting Started guide and repo';
   let setup;
 
