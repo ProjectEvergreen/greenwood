23f9cd273a02bcd934523371d5ec059985ce3dd3
Owen Buckley <owenbuckley13@gmail.com>
2020-12-20 12:52:35 -0500

    Bug/issue 443 html in markdown not rendering (#444)
    * add rehype-raw to markdown chain
    
    * update markdown test case

--- packages/cli/test/cases/build.default.markdown/build.default.markdown.spec.js
+++ packages/cli/test/cases/build.default.markdown/build.default.markdown.spec.js
@@ -45,25 +45,34 @@ describe('Build Greenwood With: ', function() {
         dom = await JSDOM.fromFile(path.resolve(this.context.publicDir, 'index.html'));
       });
     
-      it('should correctly rendering an h3 tag', function() {
+      it('should correctly rendering an <h3> tag', function() {
         const heading = dom.window.document.querySelectorAll('body h3');
         
         expect(heading.length).to.equal(1);
         expect(heading[0].textContent).to.equal('Greenwood Markdown Test');
       });
     
-      it('should correctly render a p tag', function() {
+      it('should correctly render a <p> tag', function() {
         const paragraph = dom.window.document.querySelectorAll('body p');
       
         expect(paragraph.length).to.equal(1);
         expect(paragraph[0].textContent).to.be.equal('This is some markdown being rendered by Greenwood.');
       });
 
-      it('should correctly rendering markdown with a code block', function() {
+      it('should correctly render markdown with a <code> tag', function() {
         const code = dom.window.document.querySelectorAll('body pre code');
         
         expect(code.length).to.equal(1);
         expect(code[0].textContent).to.contain('console.log(\'hello world\');');
+      });
+
+      it('should correctly render markdown with an HTML <img> tag in it', function() {
+        const images = dom.window.document.querySelectorAll('body img');
+        const myImage = images[0];
+        
+        expect(images.length).to.equal(1);
+        expect(myImage.src).to.contain('my-image.png');
+        expect(myImage.alt).to.equal('just passing by');
       });
     });
 
