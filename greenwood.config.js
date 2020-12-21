ede711328f10d177010b0d9bc354485e510c077d
HutchGrant <h.g.utchinson@gmail.com>
2020-12-20 22:54:58 -0500

    fix: disable pre/post html transforms by default

--- greenwood.config.js
+++ greenwood.config.js
@@ -34,8 +34,8 @@ module.exports = {
   plugins: [
     ...tsTransformPlugin(),
     ...sassTransformPlugin(),
-    ...preProcessHTMLTransformPlugin(),
-    ...postProcessHTMLTransformPlugin()
+    // ...preProcessHTMLTransformPlugin(),
+    // ...postProcessHTMLTransformPlugin()
   ],
   markdown: {
     plugins: [
