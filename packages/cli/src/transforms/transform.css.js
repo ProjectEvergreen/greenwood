5d97a05c499764dc82c9d99ae45febd08f9b7da9
Owen Buckley <owenbuckley13@gmail.com>
2020-12-30 11:55:16 -0500

    Enhancement/issue 426 restore userland postcss (#453)
    * user postcss plugins for production
    
    * userland postcss from css transform
    
    * restore nested styles for components
    
    * renable and update PostCSS test case
    
    * reference projectDirectory context in rollup
    
    * update build docs for PostCSS configuration
    
    * fix link

--- packages/cli/src/transforms/transform.css.js
+++ packages/cli/src/transforms/transform.css.js
@@ -1,5 +1,6 @@
+const fs = require('fs');
 const path = require('path');
-const { promises: fsp } = require('fs');
+const postcss = require('postcss');
 const TransformInterface = require('./transform.interface');
 
 class CSSTransform extends TransformInterface {
@@ -18,8 +19,21 @@ class CSSTransform extends TransformInterface {
           ? path.join(process.cwd(), url)
           : path.join(this.workspace, url);
         
-        const css = await fsp.readFile(cssPath, 'utf-8');
+        let css = await fs.promises.readFile(cssPath, 'utf-8');
         let body = '', contentType = '';
+
+        // TODO try and use context.projectDirectory
+        if (fs.existsSync(path.join(process.cwd(), 'postcss.config.js'))) {
+          const userPostcssConfig = require(`${process.cwd()}/postcss.config`);
+          const userPostcssPlugins = userPostcssConfig.plugins || [];
+          
+          if (userPostcssPlugins.length > 0) {
+            const result = await postcss(userPostcssPlugins)
+              .process(css, { from: cssPath });
+
+            css = result.css;
+          }
+        }
 
         // <style> tag used
         if (destHeader === 'style') {
