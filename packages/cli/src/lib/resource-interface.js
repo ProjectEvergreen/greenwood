9a2defd9ec99ce647865eda3cbddcd055e0e4eff
Owen Buckley <owenbuckley13@gmail.com>
2021-01-30 15:36:47 -0500

    sync ResourceInterface docs

--- packages/cli/src/lib/resource-interface.js
+++ packages/cli/src/lib/resource-interface.js
@@ -8,46 +8,56 @@ class ResourceInterface {
     this.contentType = '';
   }
 
-  // resolve relative URLs from the browser to absolute path on disk
+  // test if this plugin should change a relative URL from the browser to an absolute path on disk 
+  // like for node_modules/ resolution, not commonly needed
+  // return true | false
   // eslint-disable-next-line no-unused-vars
   async shouldResolve(url) {
     return Promise.resolve(false);
   }
 
+  // return an absolute path
   async resolve(url) {
     return Promise.resolve(url);
   }
 
-  // introduce a new resource type to the browser, on the fly, ex: `<script src="index.ts">`
+  // test if this plugin should be used to process a given url / header combo the browser and retu
+  // ex: `<script type="module" src="index.ts">`
+  // return true | false
   // eslint-disable-next-line no-unused-vars
   async shouldServe(url, headers) {
     return Promise.resolve(this.extensions.indexOf(path.extname(url)) >= 0);
   }
 
+  // return the new body and / or contentType, e.g. convert file.foo -> .js
   // eslint-disable-next-line no-unused-vars
   async serve(url, headers) {
     return Promise.resolve({});
   }
 
-  // handle an already resolved / served resource
+  // test if this plugin should return a new body for an already resolved resource
+  // useful for modifying code on the fly without needing to touch the file
+  // return true | false
   // eslint-disable-next-line no-unused-vars
   async shouldIntercept(url, body, headers) {
     return Promise.resolve(false);
   }
 
+  // return the new body
   // eslint-disable-next-line no-unused-vars
   async intercept(url, body, headers) {
     return Promise.resolve(body);
   }
 
-  // handle a (final) resource type post build, pre optimize, 
-  // ex: remove es shim <script>, convert .ts -> .js and update path references 
-  // this is only an _index.html_ file, BYOA (Bring Your Own AST)
+  // test if this plugin should manipulate any files prior to any final optmizations happening 
+  // ex: add a "banner" to all .js files with a timestamp of the build
+  // return true | false
   // eslint-disable-next-line no-unused-vars
   async shouldOptimize(url, body) {
     return Promise.resolve(false);
   }
 
+  // return the new body
   // eslint-disable-next-line no-unused-vars
   async optimize (url, body) {
     return Promise.resolve(body);
