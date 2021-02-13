8a49f991f1f154896c4bcf37457c436f0f4b914f
Owen Buckley <owenbuckley13@gmail.com>
2021-02-13 13:35:00 -0500

    add specs

--- packages/cli/test/cases/build.default.workspace-template-page/build.default.workspace-template-page.spec.js
+++ packages/cli/test/cases/build.default.workspace-template-page/build.default.workspace-template-page.spec.js
@@ -1,6 +1,6 @@
 /*
  * Use Case
- * Run Greenwood build command with no config and custom page template.
+ * Run Greenwood build command with no config and custom page and app template.
  *
  * User Result
  * Should generate a bare bones Greenwood build with custom page template.
@@ -13,7 +13,18 @@
  *
  * User Workspace
  * src/
+ *   scripts/
+ *     app-template-one.js
+ *     app-template-two.js
+ *     page-template-one.js
+ *     page-template-two.js
+  *   styles/
+ *     app-template-one.css
+ *     app-template-two.css
+ *     page-template-one.css
+ *     page-template-two.css
  *   templates/
+ *     app.html
  *     page.html
  */
 const expect = require('chai').expect;
@@ -27,7 +38,7 @@ describe('Build Greenwood With: ', function() {
   let setup;
 
   before(async function() {
-    setup = new TestBed();
+    setup = new TestBed(true);
     this.context = await setup.setupTestBed(__dirname);
   });
 
@@ -37,7 +48,6 @@ describe('Build Greenwood With: ', function() {
     });
 
     // TODO runSmokeTest(['public', 'index', 'not-found', 'hello'], LABEL);
-    runSmokeTest(['public', 'index'], LABEL);
 
     describe('Custom Page Template', function() {
       let dom;
@@ -54,6 +64,43 @@ describe('Build Greenwood With: ', function() {
         const customElement = dom.window.document.querySelectorAll('div.owen-test');
 
         expect(customElement.length).to.equal(1);
+      });
+
+      describe('merging <head> tags', function() {
+        let scriptTags;
+        let linkTags;
+
+        before(function() {
+          scriptTags = dom.window.document.querySelectorAll('head > script');
+          linkTags = dom.window.document.querySelectorAll('head > link');
+        });
+
+        it('should have 4 <script> tags in the <head>', function() {
+          expect(scriptTags.length).to.equal(4);
+        });
+
+        it('should have 4 <link> tags in the <head>', function() {
+          expect(scriptTags.length).to.equal(4);
+        });
+
+        it('should merge page template <script> tags after app template <script> tags', function() {
+          expect(scriptTags[0].src).to.match(/app-template-one.*.js/);
+          expect(scriptTags[1].src).to.match(/app-template-two.*.js/);
+          expect(scriptTags[2].src).to.match(/page-template-one.*.js/);
+          expect(scriptTags[3].src).to.match(/page-template-two.*.js/);
+        });
+
+        it('should merge page template <link> tags after app template <link> tags', function() {
+          expect(linkTags[0].href).to.match(/app-template-one.*.css/);
+          expect(linkTags[1].href).to.match(/app-template-two.*.css/);
+          expect(linkTags[2].href).to.match(/page-template-one.*.css/);
+          expect(linkTags[3].href).to.match(/page-template-two.*.css/);
+        });
+
+        // TODO
+        xit('should merge page template <style> tags after app template <style> tags', function() {
+
+        });
       });
     });
 
