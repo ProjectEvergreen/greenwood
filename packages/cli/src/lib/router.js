d5cbe4bc5af3b69c028dececd51c4f974dc28c87
Owen Buckley <owenbuckley13@gmail.com>
2021-03-24 21:01:38 -0400

    filter only for anchor tags

--- packages/cli/src/lib/router.js
+++ packages/cli/src/lib/router.js
@@ -2,21 +2,23 @@
 document.addEventListener('click', function(e) {
   e.preventDefault();
 
-  console.debug('linked clicked was...', e.path[0].href);
-  const target = e.path[0];
-  const route = target.href.replace(window.location.origin, '');
-  const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
-    return outlet.getAttribute('data-route') === route;
-  })[0];
+  if (e.path[0].href) {
+    console.debug('linked clicked was...', e.path[0].href);
+    const target = e.path[0];
+    const route = target.href.replace(window.location.origin, '');
+    const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
+      return outlet.getAttribute('data-route') === route;
+    })[0];
 
-  console.debug('routerOutlet', routerOutlet);
+    console.debug('routerOutlet', routerOutlet);
 
-  if (routerOutlet.getAttribute('data-template') === window.__greenwood.currentTemplate) {
-    window.__greenwood.currentTemplate = routerOutlet.getAttribute('data-template');
-    routerOutlet.loadRoute();
-  } else {
-    console.debug('new template detected, should do a hard reload');
-    window.location.href = target;
+    if (routerOutlet.getAttribute('data-template') === window.__greenwood.currentTemplate) {
+      window.__greenwood.currentTemplate = routerOutlet.getAttribute('data-template');
+      routerOutlet.loadRoute();
+    } else {
+      console.debug('new template detected, should do a hard reload');
+      window.location.href = target;
+    }
   }
 });
 
