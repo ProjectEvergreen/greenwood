d2907bbc39ca6db0c9519a0aff870f609672c873
Owen Buckley <owenbuckley13@gmail.com>
2021-04-28 21:04:21 -0400

    enable inline mode

--- greenwood.config.js
+++ greenwood.config.js
@@ -11,6 +11,7 @@ const FAVICON_HREF = '/assets/favicon.ico';
 module.exports = {
   workspace: path.join(__dirname, 'www'),
   mode: 'mpa',
+  optimization: 'inline',
   title: 'Greenwood',
   meta: [
     { name: 'description', content: META_DESCRIPTION },
