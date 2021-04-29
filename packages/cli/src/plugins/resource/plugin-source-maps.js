8f37f3b3b188be5db30bf68bcd2d83856dc80f00
Owen Buckley <owenbuckley13@gmail.com>
2021-04-29 19:59:28 -0400

    add support for resolving sourcemaps

---
+++ packages/cli/src/plugins/resource/plugin-source-maps.js
@@ -0,0 +1,40 @@
+/*
+ * 
+ * Detects and fully resolve requests to source map (.map) files.
+ *
+ */
+const fs = require('fs');
+const path = require('path');
+const { ResourceInterface } = require('../../lib/resource-interface');
+
+class SourceMapsResource extends ResourceInterface {
+  constructor(compilation, options) {
+    super(compilation, options);
+    this.extensions = ['.map'];
+  }
+
+  async shouldServe(url) {
+    return Promise.resolve(path.extname(url) === this.extensions[0]);
+  }
+
+  async serve(url) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const sourceMap = fs.readFileSync(url, 'utf-8');
+        
+        resolve({
+          body: sourceMap,
+          contentType: 'application/json'
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
+
+module.exports = {
+  type: 'resource',
+  name: 'plugin-source-maps',
+  provider: (compilation, options) => new SourceMapsResource(compilation, options)
+};
