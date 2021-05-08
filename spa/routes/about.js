d7dc2a49d879826b83ab51795222e90a109c2a8c
Owen Buckley <owenbuckley13@gmail.com>
2021-05-08 11:24:17 -0400

    remove demo code

--- spa/routes/about.js
+++
@@ -1,9 +0,0 @@
-import { html, LitElement } from 'lit-element';
-
-class AppAbout extends LitElement {
-  render() {
-    return html`<h1>About</h1>`;
-  }
-}
-
-customElements.define('app-about', AppAbout);
--- spa/routes/home.js
+++
@@ -1,9 +0,0 @@
-import { html, LitElement } from 'lit-element';
-
-class AppHome extends LitElement {
-  render() {
-    return html`<h1>Home</h1>`;
-  }
-}
-
-customElements.define('app-home', AppHome);
