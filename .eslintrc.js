d5c439b3ea1383ca07a19c8899afc9498b61e23d
Owen Buckley <owenbuckley13@gmail.com>
2021-07-06 14:02:00 -0400

    use eslint for TS files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
+  plugins: [
+    '@typescript-eslint'
+  ],
   env: {
     browser: true,
     node: false
