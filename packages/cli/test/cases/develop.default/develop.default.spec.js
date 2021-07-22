7fa969a573043092661ac7c107f9b223987ea039
Owen Buckley <owenbuckley13@gmail.com>
2021-07-22 12:20:47 -0400

    full suite of develop command related specs

--- packages/cli/test/cases/develop.default/develop.default.spec.js
+++ packages/cli/test/cases/develop.default/develop.default.spec.js
@@ -17,6 +17,9 @@
  *
  * User Workspace
  * src/
+ *   assets/
+ *     logo.png
+ *     source-sans-pro.woff
  *   components/
  *     header.js
  *   pages/
@@ -75,6 +78,10 @@ describe('Develop Greenwood With: ', function() {
         `${process.cwd()}/node_modules/lit-html/lib/*.js`,
         `${outputPath}/node_modules/lit-html/lib/`
       );
+      const litHtmlSourceMap = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-html/lit-html.js.map`,
+        `${outputPath}/node_modules/lit-html/`
+      );
 
       await runner.setup(outputPath, [
         ...getSetupFiles(outputPath),
@@ -83,7 +90,8 @@ describe('Develop Greenwood With: ', function() {
         ...litElementLibs,
         ...litHtmlPackageJson,
         ...litHtml,
-        ...litHtmlLibs
+        ...litHtmlLibs,
+        ...litHtmlSourceMap
       ]);
 
       return new Promise(async (resolve) => {
@@ -98,7 +106,7 @@ describe('Develop Greenwood With: ', function() {
     runSmokeTest(['serve'], LABEL);
 
     describe('Develop command specific HTML behaviors', function() {
-      let response = '';
+      let response = {};
       let dom;
 
       before(async function() {
@@ -113,13 +121,18 @@ describe('Develop Greenwood With: ', function() {
               reject();
             }
 
-            response.status = res.statusCode;
-            response.headers = res.headers;
-
+            response = res;
+            
             dom = new JSDOM(body);
-            resolve(response);
+            resolve();
           });
         });
+      });
+
+      it('should return a 200', function(done) {
+        expect(response.statusCode).to.equal(200);
+
+        done();
       });
 
       it('should return an import map shim <script> in the <head> of the document', function(done) {
@@ -156,28 +169,25 @@ describe('Develop Greenwood With: ', function() {
     });
 
     describe('Develop command specific JavaScript behaviors', function() {
-      let response = {
-        body: '',
-        code: 0
-      };
+      let response = {};
 
       before(async function() {
         return new Promise((resolve, reject) => {
-          request.get(`${hostname}:${port}/components/header.js`, (err, res) => {
+          request.get(`${hostname}:${port}/components/header.js`, (err, res, body) => {
             if (err) {
               reject();
             }
 
-            response.status = res.statusCode;
-            response.headers = res.headers;
+            response = res;
+            response.body = body;
 
-            resolve(response);
+            resolve();
           });
         });
       });
 
       it('should return a 200 status', function(done) {
-        expect(response.status).to.equal(200);
+        expect(response.statusCode).to.equal(200);
         done();
       });
 
@@ -185,31 +195,33 @@ describe('Develop Greenwood With: ', function() {
         expect(response.headers['content-type']).to.equal('text/javascript');
         done();
       });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.contain('class HeaderComponent extends HTMLElement');
+        done();
+      });
     });
 
     describe('Develop command specific CSS behaviors', function() {
-      let response = {
-        body: '',
-        code: 0
-      };
+      let response = {};
 
       before(async function() {
         return new Promise((resolve, reject) => {
-          request.get(`${hostname}:${port}/styles/main.css`, (err, res) => {
+          request.get(`${hostname}:${port}/styles/main.css`, (err, res, body) => {
             if (err) {
               reject();
             }
 
-            response.status = res.statusCode;
-            response.headers = res.headers;
+            response = res;
+            response.body = body;
 
-            resolve(response);
+            resolve();
           });
         });
       });
 
       it('should eturn a 200 status', function(done) {
-        expect(response.status).to.equal(200);
+        expect(response.statusCode).to.equal(200);
         done();
       });
 
@@ -217,23 +229,61 @@ describe('Develop Greenwood With: ', function() {
         expect(response.headers['content-type']).to.equal('text/css');
         done();
       });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.contain('color: blue;');
+        done();
+      });
     });
 
-    describe('Develop command specific node modules resolution behavior', function() {
-      let response = {
-        body: '',
-        code: 0
-      };
+    describe('Develop command with image (png) specific behavior', function() {
+      const ext = 'png';
+      let response = {};
 
       before(async function() {
         return new Promise((resolve, reject) => {
-          request.get(`${hostname}:${port}/node_modules/lit-html/lit-html.js`, (err, res) => {
+
+          request.get(`${hostname}:${port}/assets/logo.${ext}`, (err, res, body) => {
             if (err) {
               reject();
             }
 
-            response.status = res.statusCode;
-            response.headers = res.headers;
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 200 status', function(done) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.equal(`image/${ext}`);
+        done();
+      });
+
+      it('should return binary data', function(done) {
+        expect(response.body).to.contain('PNG');
+        done();
+      });
+    });
+
+    describe('Develop command with image (ico) specific behavior', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/assets/favicon.ico`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
 
             resolve(response);
           });
@@ -241,7 +291,179 @@ describe('Develop Greenwood With: ', function() {
       });
 
       it('should return a 200 status', function(done) {
-        expect(response.status).to.equal(200);
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.equal('image/x-icon');
+        done();
+      });
+
+      it('should return binary data', function(done) {
+        expect(response.body).to.contain('\u0000');
+        done();
+      });
+    });
+
+    describe('Develop command with SVG specific behavior', function() {
+      const ext = 'svg';
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/assets/webcomponents.${ext}`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 200 status', function(done) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.equal(`image/${ext}+xml`);
+        done();
+      });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body.indexOf('<svg')).to.equal(0);
+        done();
+      });
+    });
+
+    describe('Develop command with font specific (.woff) behavior', function() {
+      const ext = 'woff';
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/assets/source-sans-pro.woff`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 200 status', function(done) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.equal(ext);
+        done();
+      });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.contain('wOFF');
+        done();
+      });
+    });
+
+    describe('Develop command with JSON specific behavior', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/assets/data.json`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = JSON.parse(body);
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 200 status', function(done) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.contain('application/json');
+        done();
+      });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body.name).to.equal('Marvin');
+        done();
+      });
+    });
+
+    describe('Develop command with source map specific behavior', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/node_modules/lit-html/lit-html.js.map`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 200 status', function(done) {
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.contain('application/json');
+        done();
+      });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.contain('"sources":["src/lit-html.ts"]');
+        done();
+      });
+    });
+
+    describe('Develop command specific node modules resolution behavior', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/node_modules/lit-html/lit-html.js`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 200 status', function(done) {
+        expect(response.statusCode).to.equal(200);
         done();
       });
 
@@ -249,37 +471,80 @@ describe('Develop Greenwood With: ', function() {
         expect(response.headers['content-type']).to.equal('text/javascript');
         done();
       });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.contain('Copyright (c) 2017 The Polymer Project Authors');
+        done();
+      });
     });
 
-    // proxies to analogstudios.net/api/events
-    describe('Develop command with dev proxy', function() {
-      let response = {
-        body: '',
-        code: 0
-      };
+    // need some better 404 handling here (promise reject handling for assets and routes)
+    describe('Develop command with default 404 behavior', function() {
+      let response = {};
 
       before(async function() {
         return new Promise((resolve, reject) => {
-          request.get(`${hostname}:${port}/api/events`, (err, res) => {
+          request.get(`${hostname}:${port}/abc.js`, (err, res, body) => {
             if (err) {
               reject();
             }
 
-            response.status = res.statusCode;
-            response.headers = res.headers;
+            response = res;
+            response.body = body;
 
-            resolve(response);
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 404 status', function(done) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+
+      it('should return the correct content type', function(done) {
+        expect(response.headers['content-type']).to.contain('text/plain');
+        done();
+      });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.contain('Not Found');
+        done();
+      });
+    });
+
+    // proxies to analogstudios.net/api/events vis greenwood.config.js
+    // ideally should find something else to avoid using something "live" in our tests
+    describe('Develop command with dev proxy', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}:${port}/api/events`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = JSON.parse(body);
+
+            resolve();
           });
         });
       });
 
       it('should return a 200 status', function(done) {
-        expect(response.status).to.equal(200);
+        expect(response.statusCode).to.equal(200);
         done();
       });
 
       it('should return the correct content type', function(done) {
         expect(response.headers['content-type']).to.contain('application/json');
+        done();
+      });
+
+      it('should return the correct response body', function(done) {
+        expect(response.body).to.have.lengthOf.at.least(0);
         done();
       });
     });
