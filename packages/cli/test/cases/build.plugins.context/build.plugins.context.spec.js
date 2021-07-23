d523f7d32f56ac045805d8f8b22d775ff4e1997a
Owen Buckley <owenbuckley13@gmail.com>
2021-07-23 19:11:05 -0400

    add specs for referencing JS and CSS from a theme pack

--- packages/cli/test/cases/build.plugins.context/build.plugins.context.spec.js
+++ packages/cli/test/cases/build.plugins.context/build.plugins.context.spec.js
@@ -1,41 +1,32 @@
 /*
  * Use Case
- * Run Greenwood with a custom context plugin that provides custom templates and resources.  (e.g. a theme pack)
- * Uaer Result
- * Should generate a bare bones Greenwood build with expected custom file (.foo) behavior.
+ * Run Greenwood with a custom context plugin that provides custom templates (app / page) and resources (JS / CSS); aka a "theme pack".
+ *
+ * User Result
+ * Should generate a bare bones Greenwood build with expected templates being used from node_modules along with JS and CSS.
  *
  * User Command
  * greenwood build
  *
  * User Config
- * class FooResource extends ResourceInterface {
- *   // see complete implementation in the greenwood.config.js file used for this spec
- * }
- * 
- * {
- *   plugins: [{
- *     type: 'resource',
- *     name: 'plugin-foo',
- *     provider: (compilation, options) => new FooResource(compilation, options)
- *   }]
- * }
+ * Mock Theme Pack Plugin (from fixtures)
  *
  * Custom Workspace
  * src/
  *   pages/
- *     index.html
- *   foo-files/
- *     my-custom-file.foo
- *     my-other-custom-file.foo
+ *     slides/
+ *       index.md
+ *     index.md
  */
 const expect = require('chai').expect;
+const glob = require('glob-promise');
 const { JSDOM } = require('jsdom');
 const path = require('path');
 const { getSetupFiles, getDependencyFiles, getOutputTeardownFiles } = require('../../../../../test/utils');
 const Runner = require('gallinago').Runner;
 
 describe('Build Greenwood With: ', function() {
-  const LABEL = 'Custom Context Plugin and Default Workspace (Theme Pack)';
+  const LABEL = 'Custom Context Plugin and Default Workspace (aka Theme Packs)';
   const cliPath = path.join(process.cwd(), 'packages/cli/src/index.js');
   const outputPath = __dirname;
   let runner;
@@ -50,17 +41,31 @@ describe('Build Greenwood With: ', function() {
   describe(LABEL, function() {
 
     before(async function() {
+      // copy fixtures into node_modules
+      // to match the location specified in the plugin under test
       const themePacktemplates = await getDependencyFiles(
-        `${__dirname}/fixtures/layouts/*.html`, 
+        `${__dirname}/fixtures/layouts/*.html`,
         `${outputPath}/node_modules/my-theme-pack/dist/layouts`
+      );
+      const themePackStyles = await getDependencyFiles(
+        `${__dirname}/fixtures/styles/*.css`,
+        `${outputPath}/node_modules/my-theme-pack/dist/styles`
+      );
+      const themePackComponents = await getDependencyFiles(
+        `${__dirname}/fixtures/components/*.js`,
+        `${outputPath}/node_modules/my-theme-pack/dist/components`
       );
 
       await runner.setup(outputPath, [
         ...getSetupFiles(outputPath),
-        ...themePacktemplates
+        ...themePacktemplates,
+        ...themePackStyles,
+        ...themePackComponents
       ]);
       await runner.runCommand(cliPath, 'build');
     });
+
+    runSmokeTest(['public', 'index'], LABEL);
 
     describe('Custom Default App and Page Templates', function() {
       let dom;
@@ -78,7 +83,7 @@ describe('Build Greenwood With: ', function() {
       it('should have expected text from from a mock package layout/page.html in node_modules/', function() {
         const pageTemplateHeading = dom.window.document.querySelectorAll('body h2')[0];
 
-        expect(pageTemplateHeading.textContent).to.be.equal('This is a custom page template from the custom layouts directory.');
+        expect(pageTemplateHeading.textContent).to.be.equal('This is a custom (default) page template from the custom layouts directory.');
       });
 
       it('should have expected text from user workspace pages/index.md', function() {
@@ -115,6 +120,55 @@ describe('Build Greenwood With: ', function() {
 
         expect(pageHeadingPrimary.textContent).to.be.equal('Title Page');
         expect(pageHeadingSecondary.textContent).to.be.equal('Some content from title.md.');
+      });
+    });
+
+    describe('Custom Theme Pack theme.css in app template', function() {
+      let dom;
+
+      before(async function() {
+        dom = await JSDOM.fromFile(path.resolve(this.context.publicDir, 'index.html'));
+      });
+
+      it('should output a single CSS file for custom theme pack styles/theme.css', async function() {
+        expect(await glob.promise(path.join(this.context.publicDir, '**/theme.*.css'))).to.have.lengthOf(1);
+      });
+
+      it('should have expected link tag in the head', function() {
+        const linkTag = Array.from(dom.window.document.querySelectorAll('head link'))
+          .filter((linkTag) => {
+            return linkTag.getAttribute('rel') === 'stylesheet'
+              && linkTag.getAttribute('href').indexOf('/theme.') === 0;
+          });
+
+        expect(linkTag.length).to.equal(1);
+      });
+    });
+
+    describe('Custom Theme Pack greeting.js in custom title page template', function() {
+      let dom;
+
+      before(async function() {
+        dom = await JSDOM.fromFile(path.resolve(this.context.publicDir, 'slides/index.html'));
+      });
+
+      it('should output a single JS file for custom theme pack components/greeting.js', async function() {
+        expect(await glob.promise(path.join(this.context.publicDir, '**/greeting.*.js'))).to.have.lengthOf(1);
+      });
+
+      it('should have expected link tag in the head', function() {
+        const scriptTag = Array.from(dom.window.document.querySelectorAll('head script'))
+          .filter((linkTag) => {
+            return linkTag.getAttribute('src').indexOf('/greeting.') === 0;
+          });
+
+        expect(scriptTag.length).to.equal(1);
+      });
+
+      it('should have expected greeting component content', function() {
+        const greetingComponent = dom.window.document.querySelector('body x-greeting p');
+
+        expect(greetingComponent.textContent).to.equal('Hello from the greeting component!');
       });
     });
   });
---
+++ packages/cli/test/cases/build.plugins.context/fixtures/components/greeting.js
@@ -0,0 +1,18 @@
+const template = document.createElement('template');
+      
+template.innerHTML = `
+  <p>Hello from the greeting component!</p>
+`;
+
+class GreetingComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  async connectedCallback() {
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+customElements.define('x-greeting', GreetingComponent);
