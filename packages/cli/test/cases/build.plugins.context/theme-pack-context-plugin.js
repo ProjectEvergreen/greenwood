8fd397092844a0e6b1ce9da6ad39fa1387485ae9
Owen Buckley <owenbuckley13@gmail.com>
2021-08-03 11:50:55 -0400

    update mock example

--- packages/cli/test/cases/build.plugins.context/theme-pack-context-plugin.js
+++ packages/cli/test/cases/build.plugins.context/theme-pack-context-plugin.js
@@ -1,13 +1,15 @@
 const path = require('path');
+const packageJson = require('./package.json');
 
 module.exports = (options = {}) => [{
   type: 'context',
   name: 'my-theme-pack:context',
   provider: (compilation) => {
-    const { context } = compilation;
+    const baseDistDir = `node_modules/${packageJson.name}/dist`;
+    const { projectDirectory } = compilation.context;
     const templateLocation = options.__isDevelopment // eslint-disable-line no-underscore-dangle
       ? path.join(process.cwd(), 'fixtures/layouts')
-      : path.join(context.projectDirectory, 'node_modules/my-theme-pack/dist/layouts');
+      : path.join(projectDirectory, `${baseDistDir}/layouts`);
 
     return {
       templates: [
