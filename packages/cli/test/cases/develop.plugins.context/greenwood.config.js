748a178ffe38c0e6dc042e32c78701f5ad2ca1e7
Owen Buckley <owenbuckley13@gmail.com>
2021-08-03 13:14:28 -0400

    develop specs for context plugin

---
+++ packages/cli/test/cases/develop.plugins.context/greenwood.config.js
@@ -0,0 +1,36 @@
+const os = require('os');
+const packageName = require('./package.json').name;
+const path = require('path');
+const myThemePackPlugin = require('./theme-pack-context-plugin');
+const { ResourceInterface } = require('@greenwood/cli/src/lib/resource-interface');
+const { spawnSync } = require('child_process');
+
+const command = os.platform() === 'win32' ? 'npm.cmd' : 'npm';
+const ls = spawnSync(command, ['ls', packageName]);
+
+class MyThemePackDevelopmentResource extends ResourceInterface {
+  constructor(compilation, options) {
+    super(compilation, options);
+    this.extensions = ['*'];
+  }
+
+  async shouldResolve(url) {
+    return Promise.resolve(url.indexOf(`/node_modules/${packageName}/`) >= 0);
+  }
+
+  async resolve(url) {
+    return Promise.resolve(url.replace(`/node_modules/${packageName}/dist/`, path.join(process.cwd(), '/fixtures/')));
+  }
+}
+
+module.exports = {
+  plugins: [
+    ...myThemePackPlugin({
+      __isDevelopment: ls.stdout.toString().indexOf('(empty)') >= 0
+    }), {
+      type: 'resource',
+      name: 'my-theme-pack:resource',
+      provider: (compilation, options) => new MyThemePackDevelopmentResource(compilation, options)
+    }
+  ]
+};
