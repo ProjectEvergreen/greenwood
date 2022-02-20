311e9a556f35bc80e2a9d27039265af440c521ff
Owen Buckley <owenbuckley13@gmail.com>
2022-02-20 11:39:07 -0500

    reuse hashing utils lib

--- packages/plugin-graphql/src/core/common.js
+++ packages/plugin-graphql/src/core/common.js
@@ -1,13 +1,4 @@
-// https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0#gistcomment-2775538
-function hashString(queryKeysString) {
-  let h = 0;
-
-  for (let i = 0; i < queryKeysString.length; i += 1) {
-    h = Math.imul(31, h) + queryKeysString.charCodeAt(i) | 0; // eslint-disable-line no-bitwise
-  }
-
-  return Math.abs(h).toString();
-}
+import { hashString } from '@greenwood/cli/src/lib/hashing-utils.js';
 
 function getQueryHash(query, variables = {}) {
   const queryKeys = query;
