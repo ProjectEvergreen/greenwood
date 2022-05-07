37ccad60f4e2a99bb3e286c8a5f0f1d1d834846c
Owen Buckley <owenbuckley13@gmail.com>
2022-05-06 21:09:54 -0400

    track partial content loading to support back button functionality

--- packages/cli/src/lib/router.js
+++ packages/cli/src/lib/router.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-underscore-dangle */
+
 document.addEventListener('click', function(e) {
   const href = (e.path && e.path[0]
     ? e.path[0].href // chrome + edge
@@ -15,17 +16,56 @@ document.addEventListener('click', function(e) {
     e.preventDefault();
     
     const route = href.replace(window.location.origin, '');
+
+    console.debug({ route });
+    console.debug(window.location.pathname);
+
     const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
       return outlet.getAttribute('data-route') === route;
     })[0];
 
     if (routerOutlet.getAttribute('data-template') === window.__greenwood.currentTemplate) {
-      window.__greenwood.currentTemplate = routerOutlet.getAttribute('data-template');
+      window.__greenwood.lastRoutes.push(window.location.pathname);
+      console.debug('current template', window.__greenwood.currentTemplate);
+      console.debug('push route', routerOutlet.getAttribute('data-route'));
+
       routerOutlet.loadRoute();
     } else {
       window.location.href = href;
     }
   }
+});
+
+window.addEventListener('popstate', event => {
+  console.debug('BACK BUTTON!');
+  console.debug({ event });
+  console.debug(window.__greenwood.lastRoutes);
+
+  if (window.__greenwood.lastRoutes.length > 0) {
+    const lastRoute = window.__greenwood.lastRoutes.pop();
+
+    console.debug('same template, reuse and just fetch route partial');
+    console.debug({ lastRoute });
+
+    const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
+      return outlet.getAttribute('data-route') === lastRoute;
+    })[0];
+
+    console.debug({ routerOutlet });
+
+    if (routerOutlet && routerOutlet.getAttribute('data-template') === window.__greenwood.currentTemplate) {
+      console.debug('same template, reuse');
+      routerOutlet.loadRoute();
+    } else {
+      console.error('OOPS!  This should not have happened.  Please open an issue on our Github repo');
+      console.debug('https://github.com/ProjectEvergreen/greenwoodpo');
+    }
+  } else {
+    console.debug('breaking template boundary, follow the browser and pass go(-1)');
+    history.go(-1);
+  }
+
+  console.debug('=====================');
 });
 
 class RouteComponent extends HTMLElement {
