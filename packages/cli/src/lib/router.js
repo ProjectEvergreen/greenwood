270e028de69bb55e9fc1e182dd8659875319b808
Owen Buckley <owenbuckley13@gmail.com>
2022-06-17 20:19:55 -0400

    router refactor to better handle hash URL changes and history navigation events (#937)
    * disable push state for hash only clicks
    
    * refactor static router
    
    * only enable router if we can client side route
    
    * handle popstate when new route also has a hash
    
    * enable staticRouter and fix back navigation issues
    
    * refactor simple forwards backwards with no hash routing support
    
    * handle hash change navigation
    
    * misc refactor
    
    * clean up dead code and update specs
    
    * fix specs
    
    * validate Safari testing
    
    * clean up console logging and leave comments behind

--- packages/cli/src/lib/router.js
+++ packages/cli/src/lib/router.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 
+// ** THIS DOES NOT WORK ON SAFARI **
+// It will just load pages as if staticRouter was not enabled
 document.addEventListener('click', function(e) {
   const href = (e.path && e.path[0]
     ? e.path[0].href // chrome + edge
@@ -15,16 +17,25 @@ document.addEventListener('click', function(e) {
   if (canClientSideRoute) {
     e.preventDefault();
 
-    const route = href.replace(window.location.origin, '');
+    const targetUrl = new URL(href, window.location.origin);
     const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
-      return outlet.getAttribute('data-route') === route;
+      return outlet.getAttribute('data-route') === targetUrl.pathname;
     })[0];
 
+    // maintain the app shell if we are navigating between pages that are built from the page template
     if (routerOutlet.getAttribute('data-template') === window.__greenwood.currentTemplate) {
-      window.__greenwood.lastRoutes.push(window.location.pathname);
-
-      routerOutlet.loadRoute();
+      
+      // only update the hash if it just the hash changing
+      // else, request and load the partial for the page, and push page to the browser history stack
+      if (targetUrl.hash !== '') {
+        location = targetUrl.hash;
+      } else {
+        routerOutlet.loadRoute();
+        history.pushState({}, '', targetUrl.pathname);
+      }
     } else {
+      // this page uses is a completely different page template from the current page
+      // so just load the new page
       window.location.href = href;
     }
   }
@@ -32,31 +43,26 @@ document.addEventListener('click', function(e) {
 
 window.addEventListener('popstate', () => {
   try {
-    if (window.__greenwood.lastRoutes.length > 0) {
-      const lastRoute = window.__greenwood.lastRoutes.pop();
-      const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
-        return outlet.getAttribute('data-route') === lastRoute;
-      })[0];
+    const targetRoute = window.location;
+    const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
+      return outlet.getAttribute('data-route') === targetRoute.pathname;
+    })[0];
 
+    if (routerOutlet) {
       routerOutlet.loadRoute();
-    } else {
-      history.go(-1);
     }
-  } catch (e) {
-    console.debug('Unexpected error trying to go back.');
-    console.error(e);
+  } catch (err) {
+    console.error('Unexpected error trying to go back.', err);
   }
 });
 
 class RouteComponent extends HTMLElement {
   loadRoute() {
-    const route = this.getAttribute('data-route');
     const key = this.getAttribute('data-key');
 
     fetch(key)
       .then(res => res.text())
       .then((response) => {
-        history.pushState(response, route, route);
         document.getElementsByTagName('router-outlet')[0].innerHTML = response;
       });
   }
