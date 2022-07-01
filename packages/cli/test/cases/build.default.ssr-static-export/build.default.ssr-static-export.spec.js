ec7e1c9adea55a716bd01ec9956e0c8bff54354c
Owen Buckley <owenbuckley13@gmail.com>
2022-06-30 22:41:58 -0400

    Feature/issue 879 static export ssr routes (#921)
    * working version of SSR static export
    
    * update comment
    
    * document static SSR export
    
    * refactor and TODOs
    
    * break pass by reference

---
+++ packages/cli/test/cases/build.default.ssr-static-export/build.default.ssr-static-export.spec.js
@@ -0,0 +1,230 @@
+/*
+ * Use Case
+ * Run Greenwood with an SSR route that exports static HTML.
+ *
+ * User Result
+ * Should generate a bare bones Greenwood build with a statically rendered from server routes.
+ *
+ * User Command
+ * greenwood build
+ *
+ * User Config
+ * None
+ *
+ * User Workspace
+ *  src/
+ *   components/
+ *     counter.js
+ *     footer.js
+ *   pages/
+ *     artists.js
+ *   templates/
+ *     app.html
+ */
+import chai from 'chai';
+import fs from 'fs';
+import { JSDOM } from 'jsdom';
+import path from 'path';
+import { getSetupFiles, getDependencyFiles, getOutputTeardownFiles } from '../../../../../test/utils.js';
+import { runSmokeTest } from '../../../../../test/smoke-test.js';
+import { Runner } from 'gallinago';
+import { fileURLToPath, URL } from 'url';
+
+const expect = chai.expect;
+
+describe('Build Greenwood With: ', function() {
+  const LABEL = 'A Server Rendered Application (SSR) that is statically exported';
+  const cliPath = path.join(process.cwd(), 'packages/cli/src/index.js');
+  const outputPath = fileURLToPath(new URL('.', import.meta.url));
+  let runner;
+
+  before(async function() {
+    this.context = { 
+      publicDir: path.join(outputPath, 'public') 
+    };
+    runner = new Runner();
+  });
+
+  describe(LABEL, function() {
+
+    before(async function() {
+      const lit = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit/*.js`, 
+        `${outputPath}/node_modules/lit/`
+      );
+      const litDecorators = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit/decorators/*.js`, 
+        `${outputPath}/node_modules/lit/decorators/`
+      );
+      const litDirectives = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit/directives/*.js`, 
+        `${outputPath}/node_modules/lit/directives/`
+      );
+      const litPackageJson = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit/package.json`, 
+        `${outputPath}/node_modules/lit/`
+      );
+      const litElement = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-element/*.js`, 
+        `${outputPath}/node_modules/lit-element/`
+      );
+      const litElementPackageJson = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-element/package.json`, 
+        `${outputPath}/node_modules/lit-element/`
+      );
+      const litElementDecorators = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-element/decorators/*.js`, 
+        `${outputPath}/node_modules/lit-element/decorators/`
+      );
+      const litHtml = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-html/*.js`, 
+        `${outputPath}/node_modules/lit-html/`
+      );
+      const litHtmlPackageJson = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-html/package.json`, 
+        `${outputPath}/node_modules/lit-html/`
+      );
+      const litHtmlDirectives = await getDependencyFiles(
+        `${process.cwd()}/node_modules/lit-html/directives/*.js`, 
+        `${outputPath}/node_modules/lit-html/directives/`
+      );
+      // lit-html has a dependency on this
+      // https://github.com/lit/lit/blob/main/packages/lit-html/package.json#L82
+      const trustedTypes = await getDependencyFiles(
+        `${process.cwd()}/node_modules/@types/trusted-types/package.json`,
+        `${outputPath}/node_modules/@types/trusted-types/`
+      );
+      const litReactiveElement = await getDependencyFiles(
+        `${process.cwd()}/node_modules/@lit/reactive-element/*.js`, 
+        `${outputPath}/node_modules/@lit/reactive-element/`
+      );
+      const litReactiveElementDecorators = await getDependencyFiles(
+        `${process.cwd()}/node_modules/@lit/reactive-element/decorators/*.js`, 
+        `${outputPath}/node_modules/@lit/reactive-element/decorators/`
+      );
+      const litReactiveElementPackageJson = await getDependencyFiles(
+        `${process.cwd()}/node_modules/@lit/reactive-element/package.json`, 
+        `${outputPath}/node_modules/@lit/reactive-element/`
+      );
+
+      await runner.setup(outputPath, [
+        ...getSetupFiles(outputPath),
+        ...lit,
+        ...litPackageJson,
+        ...litDirectives,
+        ...litDecorators,
+        ...litElementPackageJson,
+        ...litElement,
+        ...litElementDecorators,
+        ...litHtmlPackageJson,
+        ...litHtml,
+        ...litHtmlDirectives,
+        ...trustedTypes,
+        ...litReactiveElement,
+        ...litReactiveElementDecorators,
+        ...litReactiveElementPackageJson
+      ]);
+
+      return new Promise(async (resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 10000);
+
+        await runner.runCommand(cliPath, 'build');
+      });
+    });
+
+    runSmokeTest(['public', 'index'], LABEL);
+
+    let dom;
+    let artistsPageGraphData;
+
+    before(async function() {
+      const graph = JSON.parse(await fs.promises.readFile(path.join(outputPath, 'public/graph.json'), 'utf-8'));
+      const artistsHtml = await fs.promises.readFile(path.join(outputPath, 'public/artists/index.html'), 'utf-8');
+      
+      artistsPageGraphData = graph.filter(page => page.route === '/artists/')[0];
+      dom = new JSDOM(artistsHtml);
+    });
+
+    describe('Build command that tests for static HTML export from SSR route', function() {
+
+      it('should have one style tags', function() {
+        const styles = dom.window.document.querySelectorAll('head > style');
+
+        expect(styles.length).to.equal(1);
+      });
+
+      it('should have three script tags', function() {
+        const scripts = dom.window.document.querySelectorAll('head > script');
+
+        expect(scripts.length).to.equal(3);
+      });
+
+      it('should have expected SSR content from the non module script tag', function() {
+        const scripts = Array.from(dom.window.document.querySelectorAll('head > script'))
+          .filter(tag => !tag.getAttribute('type'));
+
+        expect(scripts.length).to.equal(1);
+        expect(scripts[0].textContent).to.contain('console.log');
+      });
+
+      it('should have a bundled script for the footer component', function() {
+        const footerScript = Array.from(dom.window.document.querySelectorAll('head > script[type]'))
+          .filter(script => (/footer.*[a-z0-9].js/).test(script.src));
+
+        expect(footerScript.length).to.be.equal(1);
+        expect(footerScript[0].type).to.be.equal('module');
+      });
+
+      it('should have the expected number of table rows of content', function() {
+        const rows = dom.window.document.querySelectorAll('body > table tr');
+
+        expect(rows.length).to.equal(11);
+      });
+
+      it('should have the expected <title> content in the <head>', function() {
+        const title = dom.window.document.querySelectorAll('head > title');
+
+        expect(title.length).to.equal(1);
+        expect(title[0].textContent).to.equal('/artists/');
+      });
+
+      it('should have custom metadata in the <head>', function() {
+        const metaDescription = Array.from(dom.window.document.querySelectorAll('head > meta'))
+          .filter((tag) => tag.getAttribute('name') === 'description');
+
+        expect(metaDescription.length).to.equal(1);
+        expect(metaDescription[0].getAttribute('content')).to.equal('/artists/ (this was generated server side!!!)');
+      });
+
+      it('should be a part of graph.json', function() {
+        expect(artistsPageGraphData).to.not.be.undefined;
+      });
+
+      it('should have the expected menu and index values in the graph', function() {
+        expect(artistsPageGraphData.data.menu).to.equal('navigation');
+        expect(artistsPageGraphData.data.index).to.equal(7);
+      });
+
+      it('should have expected custom data values in its graph data', function() {
+        expect(artistsPageGraphData.data.author).to.equal('Project Evergreen');
+        expect(artistsPageGraphData.data.date).to.equal('01-01-2021');
+      });
+
+      it('should append the expected <script> tag for a frontmatter import <x-counter> component', function() {
+        const componentName = 'counter';
+        const counterScript = Array.from(dom.window.document.querySelectorAll('head > script[src]'))
+          .filter((tag) => tag.getAttribute('src').indexOf(`/${componentName}.`) === 0);
+
+        expect(artistsPageGraphData.imports[0]).to.equal(`/components/${componentName}.js`);
+        expect(counterScript.length).to.equal(1);
+      });
+    });
+  });
+
+  after(function() {
+    runner.teardown(getOutputTeardownFiles(outputPath));
+  });
+
+});
---
+++ packages/cli/test/cases/build.default.ssr-static-export/src/pages/artists.js
@@ -0,0 +1,92 @@
+import fetch from 'node-fetch';
+
+async function getTemplate(compilation, route) {
+  return `
+    <html>
+      <head>
+        <title>${route}</title>
+        <meta name="description" content="${route} (this was generated server side!!!)">
+
+        <script>
+          console.log(${JSON.stringify(compilation.graph.map(page => page.title).join(''))});
+        </script>
+
+        <style>
+          * {
+            color: blue;
+          }
+
+          h1 {
+            width: 50%;
+            margin: 0 auto;
+            text-align: center;
+            color: red;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>This heading was rendered server side!</h1>
+        <content-outlet></content-outlet>
+      </body>
+    </html>
+  `;
+}
+
+async function getBody(compilation) {
+  const artists = await fetch('http://www.analogstudios.net/api/artists').then(resp => resp.json());
+  const timestamp = new Date().getTime();
+  const artistsListItems = artists
+    .filter(artist => artist.isActive === '1')
+    .map((artist) => {
+      const { id, name, bio, imageUrl } = artist;
+
+      return `
+        <tr>
+          <td>${id}</td>
+          <td>${name}</td>
+          <td>${bio}</td>
+          <td><img src="${imageUrl}"/></td>
+        </tr>
+      `;
+    });
+
+  return `
+    <body>
+      <h1>Hello from the server rendered artists page! 👋</h1>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Genre</th>
+        </tr>
+        ${artistsListItems.join('')}
+      </table>
+      <h6>Fetched at: ${timestamp}</h6>
+      <pre>
+        ${JSON.stringify(compilation.graph.map(page => page.title).join(''))}
+      </pre>
+    </body>
+  `;
+}
+
+async function getFrontmatter() {
+  return {
+    menu: 'navigation',
+    index: 7,
+    imports: [
+      '/components/counter.js'
+    ],
+    data: {
+      author: 'Project Evergreen',
+      date: '01-01-2021',
+      static: true
+    }
+  };
+}
+
+export {
+  getTemplate,
+  getBody,
+  getFrontmatter
+}; 
