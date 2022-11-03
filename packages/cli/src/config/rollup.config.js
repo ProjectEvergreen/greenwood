63a9066acc26c30b2ca5984432be0e3f43c93b51
Owen Buckley <owenbuckley13@gmail.com>
2022-11-02 20:29:07 -0400

    comment config change

--- packages/cli/src/config/rollup.config.js
+++ packages/cli/src/config/rollup.config.js
@@ -121,7 +121,7 @@ const getRollupConfig = async (compilation) => {
   }).flat();
 
   return [{
-    preserveEntrySignatures: 'strict',
+    preserveEntrySignatures: 'strict', // https://github.com/ProjectEvergreen/greenwood/pull/990
     input,
     output: { 
       dir: outputDir,
