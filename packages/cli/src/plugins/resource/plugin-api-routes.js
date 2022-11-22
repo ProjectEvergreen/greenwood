799abea487089ed1f2e766da583018473100f7c4
Owen Buckley <owenbuckley13@gmail.com>
2022-11-22 14:39:15 -0500

    Response example from API route

--- packages/cli/src/plugins/resource/plugin-api-routes.js
+++ packages/cli/src/plugins/resource/plugin-api-routes.js
@@ -21,10 +21,14 @@ class ApiRoutesResource extends ResourceInterface {
     console.debug('SERVING API => ', { url });
     // TODO stop using path / url helpers!
     const { handler } = await import(pathToFileURL(`${path.join(this.compilation.context.apisDir, this.getBareUrlPath(url).replace('/api', ''))}.js`));
-    const req = new Request(new URL(`https://localhost:1984${url}`));
+    const req = new Request(new URL(`https://localhost:1984${url}`)); // TODO can we assume localhost?
+    const resp = await handler(req);
+    const body = await resp.json(); // TODO assumes JSON
+
+    console.debug({ body });
 
     return {
-      body: await handler(req)
+      body
     };
   }
 }
--- www/api/greeting.js
+++ www/api/greeting.js
@@ -1,11 +1,7 @@
-export async function handler(request, response) {
-  console.debug({ request, response });
+export async function handler(request) {
   const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
   const name = params.has('name') ? params.get('name') : 'Greenwood';
+  const body = { message: `Hello ${name}!!!` };
 
-  console.debug({ params });
-  console.debug({ name });
-
-  // TODO use Response
-  return { message: `Hello ${name}!!!` };
+  return new Response(JSON.stringify(body));
 }
