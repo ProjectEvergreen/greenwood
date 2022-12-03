a1579777d5bd3f15b7634365afc07b31c245bee8
Owen Buckley <owenbuckley13@gmail.com>
2022-12-02 20:54:56 -0500

    add local development cache busting for API routes

--- packages/cli/src/plugins/resource/plugin-api-routes.js
+++ packages/cli/src/plugins/resource/plugin-api-routes.js
@@ -4,7 +4,6 @@
  *
  */
 import path from 'path';
-import { pathToFileURL } from 'url';
 import { ResourceInterface } from '../../lib/resource-interface.js';
 
 class ApiRoutesResource extends ResourceInterface {
@@ -20,8 +19,18 @@ class ApiRoutesResource extends ResourceInterface {
   async serve(url) {
     console.debug('SERVING API => ', { url });
     // TODO stop using path / url helpers!
-    const { handler } = await import(pathToFileURL(`${path.join(this.compilation.context.apisDir, this.getBareUrlPath(url).replace('/api', ''))}.js`));
-    const req = new Request(new URL(`https://localhost:1984${url}`)); // TODO can we assume localhost?
+    let resolvedUrl = `${path.join(this.compilation.context.apisDir, this.getBareUrlPath(url).replace('/api', ''))}.js`;
+
+    // https://github.com/nodejs/modules/issues/307#issuecomment-1165387383
+    if (process.env.__GWD_COMMAND__) { // eslint-disable-line no-underscore-dangle
+      resolvedUrl = `${resolvedUrl}?cachebust=${Date.now()}`;
+    }
+
+    const { handler } = await import(resolvedUrl);
+
+    // TODO can we assume localhost?
+    // TODO get port
+    const req = new Request(new URL(`https://localhost:1984${url}`));
     const resp = await handler(req);
     const body = await resp.json(); // TODO assumes JSON
 
--- www/api/greeting.js
+++ www/api/greeting.js
@@ -1,7 +1,7 @@
 export async function handler(request) {
   const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
   const name = params.has('name') ? params.get('name') : 'Greenwood';
-  const body = { message: `Hello ${name}!!!` };
+  const body = { message: `Hello ${name}!!!???` };
 
   return new Response(JSON.stringify(body));
 }
