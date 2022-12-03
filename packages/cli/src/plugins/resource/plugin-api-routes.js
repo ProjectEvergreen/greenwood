1582d63d2ffd043026a17ccad812937f5580fc89
Owen Buckley <owenbuckley13@gmail.com>
2022-12-02 21:06:30 -0500

    refactor out dependency on path

--- packages/cli/src/plugins/resource/plugin-api-routes.js
+++ packages/cli/src/plugins/resource/plugin-api-routes.js
@@ -3,7 +3,6 @@
  * Manages routing to API routes.
  *
  */
-import path from 'path';
 import { ResourceInterface } from '../../lib/resource-interface.js';
 
 class ApiRoutesResource extends ResourceInterface {
@@ -17,24 +16,20 @@ class ApiRoutesResource extends ResourceInterface {
   }
 
   async serve(url) {
-    console.debug('SERVING API => ', { url });
-    // TODO stop using path / url helpers!
-    let resolvedUrl = `${path.join(this.compilation.context.apisDir, this.getBareUrlPath(url).replace('/api', ''))}.js`;
+    let href = new URL(`${this.getBareUrlPath(url).replace('/api/', '')}.js`, `file://${this.compilation.context.apisDir}`).href;
 
     // https://github.com/nodejs/modules/issues/307#issuecomment-1165387383
     if (process.env.__GWD_COMMAND__) { // eslint-disable-line no-underscore-dangle
-      resolvedUrl = `${resolvedUrl}?cachebust=${Date.now()}`;
+      href = `${resolvedUrl}?t=${Date.now()}`;
     }
 
-    const { handler } = await import(resolvedUrl);
+    const { handler } = await import(href);
 
     // TODO can we assume localhost?
     // TODO get port
     const req = new Request(new URL(`https://localhost:1984${url}`));
     const resp = await handler(req);
-    const body = await resp.json(); // TODO assumes JSON
-
-    console.debug({ body });
+    const body = await resp.json(); // TODO assumes JSON, pass Raesponse all the way up?
 
     return {
       body
