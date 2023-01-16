fc56315b1ee36919da4adda01676977e0b62dca7
Owen Buckley <owenbuckley13@gmail.com>
2023-01-16 10:25:03 -0500

    all plugin specs passing

--- packages/plugin-import-commonjs/src/index.js
+++ packages/plugin-import-commonjs/src/index.js
@@ -5,7 +5,6 @@
  */
 import commonjs from '@rollup/plugin-commonjs';
 import fs from 'fs';
-import path from 'path';
 import { parse, init } from 'cjs-module-lexer';
 import { ResourceInterface } from '@greenwood/cli/src/lib/resource-interface.js';
 import rollupStream from '@rollup/stream';
@@ -13,9 +12,10 @@ import rollupStream from '@rollup/stream';
 // bit of a workaround for now, but maybe this could be supported by cjs-module-lexar natively?
 // https://github.com/guybedford/cjs-module-lexer/issues/35
 const testForCjsModule = async(url) => {
+  const { pathname } = url;
   let isCommonJs = false;
 
-  if (path.extname(url) === '.js' && (/node_modules/).test(url) && url.indexOf('es-module-shims.js') < 0) {
+  if (pathname.split('.').pop() === '.js' && pathname.startsWith('/node_modules/') && pathname.indexOf('es-module-shims.js') < 0) {
     try {
       await init();
       const body = await fs.promises.readFile(url, 'utf-8');
@@ -26,7 +26,7 @@ const testForCjsModule = async(url) => {
       const { message } = e;
       const isProbablyLexarErrorSoIgnore = message.indexOf('Unexpected import statement in CJS module.') >= 0 
         || message.indexOf('Unexpected export statement in CJS module.') >= 0;
-      
+
       if (!isProbablyLexarErrorSoIgnore) {
         // we probably _shouldn't_ ignore this, so let's log it since we don't want to swallow all errors
         console.error(e);
@@ -34,7 +34,7 @@ const testForCjsModule = async(url) => {
     }
   }
 
-  return Promise.resolve(isCommonJs);
+  return isCommonJs;
 };
 
 class ImportCommonJsResource extends ResourceInterface {
@@ -43,23 +43,16 @@ class ImportCommonJsResource extends ResourceInterface {
   }
 
   async shouldIntercept(url) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const isCommonJs = await testForCjsModule(url);
-
-        return resolve(isCommonJs);
-      } catch (e) {
-        console.error(e);
-        reject(e);
-      }
-    });
+    return await testForCjsModule(url);
   }
 
   async intercept(url) {
+    const { pathname } = url;
+    
     return new Promise(async(resolve, reject) => {
       try {
         const options = {
-          input: url,
+          input: pathname,
           output: { format: 'esm' },
           plugins: [
             commonjs()
@@ -70,10 +63,10 @@ class ImportCommonJsResource extends ResourceInterface {
 
         stream.on('data', (data) => (bundle += data));
         stream.on('end', () => {
-          console.debug(`proccessed module "${url}" as a CommonJS module type.`);
-          resolve({
-            body: bundle
-          });
+          console.debug(`processed module "${pathname}" as a CommonJS module type.`);
+          resolve(new Response(bundle, {
+            headers: response.headers
+          }));
         });
       } catch (e) {
         reject(e);
