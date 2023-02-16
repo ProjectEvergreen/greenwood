bd427e386b4ca2326bedcaeebc87e414a48ba6a3
Owen Buckley <owenbuckley13@gmail.com>
2023-02-15 22:06:55 -0500

    leverage composed path for static router cross browser event delegation support

--- packages/cli/src/lib/router.js
+++ packages/cli/src/lib/router.js
@@ -1,21 +1,24 @@
 /* eslint-disable no-underscore-dangle */
 
-// ** THIS DOES NOT WORK ON SAFARI **
-// It will just load pages as if staticRouter was not enabled
-// https://github.com/ProjectEvergreen/greenwood/issues/559
-document.addEventListener('click', async function(e) {
+document.addEventListener('click', function(e) {
   const currentUrl = window.location;
-  const href = (e.path && e.path[0]
-    ? e.path[0].href // chrome + edge
-    : e.originalTarget && e.originalTarget.href
-      ? e.originalTarget.href // firefox
-      : '') || '';
+  console.log(e);
+  console.log('PATH', e.path);
+  console.log('COMPOSED PATH', e.composedPath());
+  console.log('originalTarget', e.originalTarget);
+  // https://stackoverflow.com/questions/51493918/vanilla-javascript-event-delegation-when-dealing-with-web-components
+  const href = e.composedPath && e.composedPath()[0].tagName === 'A'
+    ? e.composedPath()[0].href
+    : '';
+
+  console.log('href', { href });
   // best case "guess" is that if the link originates on the current site when resolved by the browser
   // treat it as a client side route, ex:  /about/, /docs/ and trigger the client side router
   // https://github.com/ProjectEvergreen/greenwood/issues/562
   const isOnCurrentDomain = href.indexOf(currentUrl.hostname) >= 0 || href.indexOf('localhost') >= 0;
   const canClientSideRoute = href !== '' && isOnCurrentDomain;
 
+  console.log('canClientSideRoute', { canClientSideRoute, isOnCurrentDomain });
   if (canClientSideRoute) {
     e.preventDefault();
 
@@ -23,14 +26,16 @@ document.addEventListener('click', async function(e) {
     const routerOutlet = Array.from(document.getElementsByTagName('greenwood-route')).filter(outlet => {
       return outlet.getAttribute('data-route') === targetUrl.pathname;
     })[0];
+    console.log('targetUrl', { targetUrl });
 
     // maintain the app shell if we are navigating between pages that are built from the same page template
     // also, some routes may be SSR, so we may not always match on a static route
     if (routerOutlet && routerOutlet.getAttribute('data-template') === window.__greenwood.currentTemplate) {
+      console.log('DO THE THING!!!');
       const { hash, pathname } = targetUrl;
 
       if (currentUrl.pathname !== pathname) {
-        await routerOutlet.loadRoute();
+        routerOutlet.loadRoute();
 
         history.pushState({}, '', pathname);
       }
@@ -41,6 +46,7 @@ document.addEventListener('click', async function(e) {
     } else {
       // this page uses is a completely different page template from the current page
       // so just load the new page
+      console.log('no thing ...');
       window.location.href = href;
     }
   }
