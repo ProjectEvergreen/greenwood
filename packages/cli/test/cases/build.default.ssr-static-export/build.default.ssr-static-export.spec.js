ad14f147832565efdd9191ace439ee969e2a8200
Owen Buckley <owenbuckley13@gmail.com>
2023-04-26 08:42:46 -0400

    filter for expected script instead of hardcoding position in the array for testing

--- packages/cli/test/cases/build.default.ssr-static-export/build.default.ssr-static-export.spec.js
+++ packages/cli/test/cases/build.default.ssr-static-export/build.default.ssr-static-export.spec.js
@@ -209,8 +209,10 @@ describe('Build Greenwood With: ', function() {
         const componentName = 'counter';
         const counterScript = Array.from(dom.window.document.querySelectorAll('head > script[src]'))
           .filter((tag) => tag.getAttribute('src').indexOf(`/${componentName}.`) === 0);
+        const counterImport = artistsPageGraphData.imports
+          .filter(script => script.src && script.src.indexOf(`${componentName}.`) >= 0);
 
-        expect(artistsPageGraphData.imports[0].src).to.equal(`/components/${componentName}.js`);
+        expect(counterImport.length).to.equal(1);
         expect(counterScript.length).to.equal(1);
       });
     });
