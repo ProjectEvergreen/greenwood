2ae137bc956bec2bb552adb039f336a1d0846a6a
Owen Buckley <owenbuckley13@gmail.com>
2023-05-06 09:47:53 -0400

    Bug/issue 1102 not found static assets returning 500 for serve command (#1103)
    * check static resource exists before serving it
    
    * add test cases to cover expected 404 status code handling

--- packages/cli/test/cases/serve.default.api/serve.default.api.spec.js
+++ packages/cli/test/cases/serve.default.api/serve.default.api.spec.js
@@ -128,6 +128,30 @@ describe('Serve Greenwood With: ', function() {
         done();
       });
     });
+
+    describe('Serve command with API 404 not found behavior', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}/api/foo`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 404 status', function(done) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
   });
 
   after(function() {
--- packages/cli/test/cases/serve.default.ssr/serve.default.ssr.spec.js
+++ packages/cli/test/cases/serve.default.ssr/serve.default.ssr.spec.js
@@ -305,6 +305,30 @@ describe('Serve Greenwood With: ', function() {
         expect(scriptFiles.length).to.equal(2);
       });
     });
+
+    describe('Serve command with 404 not found behavior', function() {
+      let response = {};
+
+      before(async function() {
+        return new Promise((resolve, reject) => {
+          request.get(`${hostname}/foo`, (err, res, body) => {
+            if (err) {
+              reject();
+            }
+
+            response = res;
+            response.body = body;
+
+            resolve();
+          });
+        });
+      });
+
+      it('should return a 404 status', function(done) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
   });
 
   after(function() {
