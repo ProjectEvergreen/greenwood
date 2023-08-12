69a61ca51cd54385e3f7a75d432a0f7750e527b2
Owen Buckley <owenbuckley13@gmail.com>
2023-08-12 11:49:42 -0400

    Feature/issue 1008 vercel adapter plugin (#1139)
    * initial implementation of a vercel adapter plugin
    
    * add test case for static build output
    
    * handle request and response properties
    
    * finishing touches to README.md
    
    * add to custom plugins docs page
    
    * link to edge runtime support issue in caveats sections
    
    * handle windows pathname interop

---
+++ packages/plugin-adapter-vercel/test/cases/build.default/src/api/fragment.js
@@ -0,0 +1,27 @@
+import { renderFromHTML } from 'wc-compiler';
+import { getArtists } from '../services/artists.js';
+
+export async function handler(request) {
+  const params = new URLSearchParams(request.url.slice(request.url.indexOf('?')));
+  const offset = params.has('offset') ? parseInt(params.get('offset'), 10) : null;
+  const headers = new Headers({ 'Content-Type': 'text/html' });
+  const artists = getArtists(offset);
+  const { html } = await renderFromHTML(`
+    ${
+      artists.map((item, idx) => {
+        const { name, imageUrl } = item;
+
+        return `
+          <app-card
+            title="${offset + idx + 1}) ${name}"
+            thumbnail="${imageUrl}"
+          ></app-card>
+        `;
+      }).join('')
+    }
+  `, [
+    new URL('../components/card.js', import.meta.url)
+  ]);
+
+  return new Response(html, { headers });
+}
