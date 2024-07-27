525780d8ea7d42db7c8ac7c5362276915126b5af
Jonathan Stockdill <jon.stockdill@gmail.com>
2024-07-27 13:03:52 +0000

    Dynamic Sitemap - Oops, linting  (#1232)

--- packages/cli/src/plugins/copy/plugin-copy-sitemap.js
+++ packages/cli/src/plugins/copy/plugin-copy-sitemap.js
@@ -1,26 +1,22 @@
-import { checkResourceExists } from "@greenwood/cli/src/lib/resource-utils.js";
 import fs from 'fs/promises';
 
-
 async function writeSitemap(compilation) {
-    try {
-      const { scratchDir, projectDirectory } = compilation.context;
-      const adapterScratchUrl = new URL('./sitemap.xml', scratchDir);
-  
-      // Check if module exists
-      const sitemapModule = await import(`${projectDirectory}/src/sitemap.xml.js`);
-      const sitemap = await sitemapModule.generateSitemap(compilation);
-  
-      await fs.writeFile(adapterScratchUrl, sitemap);
-      console.info('Wrote sitemap to ./sitemap.xml');
+  try {
+    const { scratchDir, projectDirectory } = compilation.context;
+    const adapterScratchUrl = new URL('./sitemap.xml', scratchDir);
 
-      return adapterScratchUrl;
-    } catch (error) {
-      console.error('Error in sitemapAdapter:', error);
-    }
+    // Check if module exists
+    const sitemapModule = await import(`${projectDirectory}/src/sitemap.xml.js`);
+    const sitemap = await sitemapModule.generateSitemap(compilation);
+
+    await fs.writeFile(adapterScratchUrl, sitemap);
+    console.info('Wrote sitemap to ./sitemap.xml');
+
+    return adapterScratchUrl;
+  } catch (error) {
+    console.error('Error in sitemapAdapter:', error);
   }
-
-
+}
 
 const greenwoodPluginCopySitemap = [{
   type: 'copy',
@@ -31,8 +27,8 @@ const greenwoodPluginCopySitemap = [{
     const sitemapScratchUrl = await writeSitemap(compilation);
 
     return [{
-        from: sitemapScratchUrl,
-        to: new URL('./sitemap.xml', outputDir)
+      from: sitemapScratchUrl,
+      to: new URL('./sitemap.xml', outputDir)
     }];
   }
 }];
