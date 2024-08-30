d96758bc3731aa6e455e91c1294b6a3abc2973e6
Owen Buckley <owenbuckley13@gmail.com>
2024-08-30 17:19:17 -0400

    import attributes polyfill config serve test cases

--- packages/cli/test/cases/serve.config.polyfill-import-attributes/serve.config.polyfills-import-attributes.spec.js
+++ packages/cli/test/cases/serve.config.polyfill-import-attributes/serve.config.polyfills-import-attributes.spec.js
@@ -1,12 +1,12 @@
 /*
  * Use Case
- * Run Greenwood develop command with import attributes polyfill flag enabled.
+ * Run Greenwood serve command with import attributes polyfill flag enabled.
  *
  * User Result
- * Should start the development server and have all the expected import attributes polyfill behaviors.
+ * Should start the production server and have all the expected import attributes polyfill behaviors..
  *
  * User Command
- * greenwood develop
+ * greenwood serve
  *
  * User Config
  * polyfill: {
@@ -25,24 +25,28 @@
  *
  */
 import chai from 'chai';
+import fs from 'fs';
 import path from 'path';
-import { getSetupFiles } from '../../../../../test/utils.js';
+import { getSetupFiles, getOutputTeardownFiles, getDependencyFiles } from '../../../../../test/utils.js';
 import { Runner } from 'gallinago';
 import { fileURLToPath, URL } from 'url';
 
 const expect = chai.expect;
 
-describe('Develop Greenwood With: ', function() {
+describe('Serve Greenwood With: ', function() {
   const LABEL = 'Import Attributes Polyfill Configuration';
   const cliPath = path.join(process.cwd(), 'packages/cli/src/index.js');
-  const outputPath = fileURLToPath(new URL('.', import.meta.url));
-  const hostname = 'http://localhost';
-  const port = 1984;
+  const outputUrl = new URL('.', import.meta.url);
+  const outputPath = fileURLToPath(outputUrl);
+  const hostname = 'http://127.0.0.1:8080';
+  const jsHash = '8b10e832';
+  const cssHash = '57750ec5';
+  const jsonHash = '46b4fc8c';
   let runner;
 
   before(function() {
     this.context = {
-      hostname: `${hostname}:${port}`
+      hostname
     };
     runner = new Runner();
   });
@@ -50,32 +54,35 @@ describe('Develop Greenwood With: ', function() {
   describe(LABEL, function() {
 
     before(async function() {
+      const greenwoodRouterLibs = await getDependencyFiles(
+        `${process.cwd()}/packages/cli/src/lib/router.js`,
+        `${outputPath}/node_modules/@greenwood/cli/src/lib`
+      );
 
       runner.setup(outputPath, [
-        ...getSetupFiles(outputPath)
+        ...getSetupFiles(outputPath),
+        ...greenwoodRouterLibs
       ]);
+      runner.runCommand(cliPath, 'build');
 
       return new Promise((resolve) => {
         setTimeout(() => {
           resolve();
-        }, 5000);
+        }, 10000);
 
-        runner.runCommand(cliPath, 'develop', { async: true });
+        runner.runCommand(cliPath, 'serve', { async: true });
       });
     });
 
-    describe('Import Attributes Polyfill Behaviors for the initiating JavaScript file (hero.js)', function() {
+    describe('Import Attributes Polyfill Behaviors for the initiating JavaScript file (hero.js) being served and bundled', function() {
       let response = {};
       let text;
+      let contents;
 
       before(async function() {
-        response = await fetch(`${hostname}:${port}/components/hero.js`, {
-          headers: {
-            accept: 'text/html'
-          }
-        });
-
+        response = await fetch(`${hostname}/hero.${jsHash}.js`);
         text = await response.clone().text();
+        contents = await fs.promises.readFile(new URL(`./public/hero.${jsHash}.js`, outputUrl), 'utf-8');
       });
 
       it('should return the correct content type', function(done) {
@@ -89,37 +96,37 @@ describe('Develop Greenwood With: ', function() {
         done();
       });
 
-      it('should not contain import attributes syntax', function(done) {
+      it('should not contain import attributes syntax in the response or bundled output', function(done) {
         expect(text.replace(/ /g, '')).to.not.contain('with{type:');
+        expect(contents.replace(/ /g, '')).to.not.contain('with{type:');
 
         done();
       });
 
       it('should contain import attributes polyfill syntax for CSS', function(done) {
-        expect(text.replace(/ /g, '')).to.contain('importsheetfrom\'./hero.css?polyfill=type-css\'');
+        expect(text).to.contain(`import t from"/hero.${cssHash}.css?polyfill=type-css";`);
+        expect(contents).to.contain(`import t from"/hero.${cssHash}.css?polyfill=type-css";`);
 
         done();
       });
 
       it('should contain import attributes polyfill syntax for JSON', function(done) {
-        expect(text.replace(/ /g, '')).to.contain('importjsonfrom\'./hero.json?polyfill=type-json\'');
+        expect(text).to.contain(`import e from"/hero.${jsonHash}.json?polyfill=type-json";`);
+        expect(contents).to.contain(`import e from"/hero.${jsonHash}.json?polyfill=type-json";`);
 
         done();
       });
     });
 
-    describe('Import Attributes Polyfill Behavior for CSS', function() {
+    describe('Import Attributes Polyfill Behavior for CSS (hero.css) being served and bundled', function() {
       let response = {};
       let text;
+      let contents;
 
       before(async function() {
-        response = await fetch(`${hostname}:${port}/components/hero.css?polyfill=type-css`, {
-          headers: {
-            accept: 'text/html'
-          }
-        });
-
+        response = await fetch(`${hostname}/hero.${cssHash}.css?polyfill=type-css`);
         text = await response.clone().text();
+        contents = await fs.promises.readFile(new URL(`./public/hero.${cssHash}.css`, outputUrl), 'utf-8');
       });
 
       it('should return the correct content type', function(done) {
@@ -135,23 +142,21 @@ describe('Develop Greenwood With: ', function() {
 
       it('should return the contents as an exported Constructable StyleSheet ES module', function(done) {
         expect(text).to.equal('const sheet = new CSSStyleSheet();sheet.replaceSync(`:host h2{font-size:3em}`);export default sheet;');
+        expect(contents).to.equal('const sheet = new CSSStyleSheet();sheet.replaceSync(`:host h2{font-size:3em}`);export default sheet;');
 
         done();
       });
     });
 
-    describe('Import Attributes Polyfill Behavior for JSON', function() {
+    describe('Import Attributes Polyfill Behavior for JSON (hero.json) being served and bundled', function() {
       let response = {};
       let text;
+      let contents;
 
       before(async function() {
-        response = await fetch(`${hostname}:${port}/components/hero.json?polyfill=type-json`, {
-          headers: {
-            accept: 'text/html'
-          }
-        });
-
+        response = await fetch(`${hostname}/hero.${jsonHash}.json?polyfill=type-json`);
         text = await response.clone().text();
+        contents = await fs.promises.readFile(new URL(`./public/hero.${jsonHash}.json`, outputUrl), 'utf-8');
       });
 
       it('should return the correct content type', function(done) {
@@ -167,6 +172,7 @@ describe('Develop Greenwood With: ', function() {
 
       it('should return the contents as an exported Constructable StyleSheet ES module', function(done) {
         expect(text).to.equal('export default {"msg":"Hello World"}');
+        expect(contents).to.equal('export default {"msg":"Hello World"}');
 
         done();
       });
@@ -174,10 +180,7 @@ describe('Develop Greenwood With: ', function() {
   });
 
   after(function() {
+    runner.teardown(getOutputTeardownFiles(outputPath));
     runner.stopCommand();
-    runner.teardown([
-      path.join(outputPath, '.greenwood'),
-      path.join(outputPath, 'node_modules')
-    ]);
   });
 });
