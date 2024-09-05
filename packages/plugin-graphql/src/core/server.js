5be9602711e1039aecb039e8f7b0dda6d83824e5
Owen Buckley <owenbuckley13@gmail.com>
2024-09-04 21:23:47 -0400

    optimize graphql server for production builds

--- packages/plugin-graphql/src/core/server.js
+++ packages/plugin-graphql/src/core/server.js
@@ -4,19 +4,26 @@ const graphqlServer = async (compilation) => {
   const { config, graph, context } = compilation;
   const { createSchema } = await import('../schema/schema.js');
   const { createCache } = await import('./cache.js');
+  // disable playground for production builds
+  const playground = process.env.__GWD_COMMAND__ === 'develop'
+    ? {
+        endpoint: '/graphql',
+        settings: {
+          'editor.theme': 'light'
+        }
+      }
+    : {};
 
   const server = new ApolloServer({
     schema: await createSchema(compilation),
-    playground: {
-      endpoint: '/graphql',
-      settings: {
-        'editor.theme': 'light'
-      }
-    },
+    playground,
+    introspection: process.env.__GWD_COMMAND__ === 'develop',
     context: async (integrationContext) => {
       const { req } = integrationContext;
 
-      if (req.query.q !== 'internal') {
+      // make sure to ignore introspection requests from being generated as an output cache file
+      // https://stackoverflow.com/a/58040379/417806
+      if (req.query.q !== 'internal' && req.body.operationName !== 'IntrospectionQuery') {
         await createCache(req, context);
       }
 
