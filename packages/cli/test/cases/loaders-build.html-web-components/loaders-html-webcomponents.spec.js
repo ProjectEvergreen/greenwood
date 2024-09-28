439d0261fa586d9e4f61aad1b86a944437a0381e
Owen Buckley <owenbuckley13@gmail.com>
2024-09-27 20:49:52 -0400

    fix linting

--- packages/cli/test/cases/loaders-build.html-web-components/loaders-html-webcomponents.spec.js
+++ packages/cli/test/cases/loaders-build.html-web-components/loaders-html-webcomponents.spec.js
@@ -1,6 +1,7 @@
 /*
  * Use Case
  * Run Greenwood and correctly prerender "HTML" Web Components.
+ * https://blog.jim-nielsen.com/2023/html-web-components/
  *
  * User Result
  * Should generate a static Greenwood build with the expected prerender "HTML" Web Components content.
@@ -21,8 +22,6 @@
 *    index.html
  */
 import chai from 'chai';
-import fs from 'fs';
-import glob from 'glob-promise';
 import { JSDOM } from 'jsdom';
 import path from 'path';
 import { runSmokeTest } from '../../../../../test/smoke-test.js';
@@ -65,21 +64,21 @@ describe('Build Greenwood With: ', function() {
       it('should not have any <template> tags within the document', function() {
         expect(dom.window.document.querySelectorAll('template').length).to.equal(0);
       });
-  
+
       it('should only have one <app-picture-frame> tag', function() {
         expect(pictureFrame.length).to.equal(1);
       });
-  
+
       it('should have the expected title attribute content in the heading of HTML', () => {
         const heading = pictureFrame[0].querySelectorAll('.picture-frame .heading');
-  
+
         expect(heading.length).to.equal(1);
         expect(heading[0].textContent).to.equal('Greenwood');
       });
-  
+
       it('should have the expected image from userland in the HTML', () => {
         const img = pictureFrame[0].querySelectorAll('.picture-frame img');
-  
+
         expect(img.length).to.equal(1);
         expect(img[0].getAttribute('alt')).to.equal('Greenwood logo');
         expect(img[0].getAttribute('src')).to.equal('/assets/greenwood.png');
@@ -90,5 +89,4 @@ describe('Build Greenwood With: ', function() {
   after(function() {
     runner.teardown(getOutputTeardownFiles(outputPath));
   });
-
 });
