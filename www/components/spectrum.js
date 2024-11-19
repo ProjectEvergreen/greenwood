086cde208fd46d341d3573a0540825cd76fc32a0
Owen Buckley <owenbuckley13@gmail.com>
2024-11-19 09:09:20 -0500

    spectrum styles working

--- www/components/spectrum.js
+++ www/components/spectrum.js
@@ -1,5 +1,8 @@
-// import '@spectrum-web-components/theme/sp-theme.js';
-// import '@spectrum-web-components/theme/src/themes.js';
+// https://opensource.adobe.com/spectrum-web-components/tools/theme/#advanced-usage
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/src/themes.js';
+
+import '@spectrum-web-components/button/sp-button.js';
 import '@spectrum-web-components/action-menu/sp-action-menu.js';
 import '@spectrum-web-components/menu/sp-menu.js';
 import '@spectrum-web-components/menu/sp-menu-item.js';
