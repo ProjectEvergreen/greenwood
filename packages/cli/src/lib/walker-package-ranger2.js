4ba3dc80dcfb6854037642ed97209ae93e4f7856
Owen Buckley <owenbuckley13@gmail.com>
2024-11-26 15:57:42 -0500

    WIP of export map patterns refactoring

--- packages/cli/src/lib/walker-package-ranger2.js
+++ packages/cli/src/lib/walker-package-ranger2.js
@@ -1,8 +1,8 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 
 /* eslint-disable max-depth,complexity */
 const importMap = {};
-const extensionFilters = ['map', 'd.ts'];
+// TODO const extensionFilters = ['map', 'd.ts'];
 
 function updateImportMap(key, value) {
   importMap[key.replace('./', '')] = value.replace('./', '');
@@ -41,71 +41,240 @@ function derivePackageRoot(dependencyName, resolved) {
   return derived;
 }
 
-// https://nodejs.org/api/packages.html#subpath-patterns
-async function walkExportPatterns(dependency, exp, resolvedRoot) {
-  if (exp.endsWith('*')) {
-    const dir = new URL(exp.replace('*', ''), resolvedRoot);
-    const files = await fs.readdir(dir);
+/*
+ * https://nodejs.org/api/packages.html#subpath-patterns
+ *
+ * Examples
+ * "./icons/*": "./icons/*" - https://unpkg.com/browse/@spectrum-web-components/icons-workflow@1.0.1/package.json
+ * "./components/*": "./dist/components/*.js" - https://unpkg.com/browse/@uswds/web-components@0.0.1-alpha/package.json
+ * "./src/components/*": "./src/components/* /index.js - https://unpkg.com/browse/@uswds/web-components@0.0.1-alpha/package.json
+ */
 
-    files
-      .filter((file) => {
-        let shouldNotFilter = true;
+// Helper function to match patterns using regular expressions (thanks ChatGPT)
+function matchPattern(file, pattern) {
+  const regexPattern = globToRegex(pattern);
 
-        extensionFilters.forEach((extFilter) => {
-          if (file.endsWith(extFilter)) {
-            shouldNotFilter = false;
-          }
-        });
-
-        return shouldNotFilter;
-      })
-      .forEach((file) => {
-        updateImportMap(`${dependency}/${exp.replace('/*', '')}/${file}`, `/node_modules/${dependency}/${exp.replace('/*', '')}/${file}`);
-      });
-  } else {
-    // TODO
-    // "./feature/*": "./feature/*.js",
-  }
+  return regexPattern.test(file);
 }
 
+// Helper function to convert glob pattern to regex (thanks ChatGPT)
+function globToRegex(pattern) {
+  // Escape special regex characters
+  pattern = pattern.replace(/[.+^${}()|[\]\\]/g, '\\$&');
+
+  // Replace glob `*` with regex `[^/]*` (any characters except slashes)
+  pattern = pattern.replace(/\*/g, '[^/]*');
+
+  // Return the final regex
+  return new RegExp('^' + pattern + '$');
+}
+
+async function walkExportPatterns(dependency, sub, subValue, resolvedRoot) {
+  // console.log('walkExportPatterns', { dependency, sub, subValue });
+  const segments = subValue.split('/').filter((segment) => segment !== '.');
+  let startingSegmentOffset = '';
+
+  // find the "deepest" segment we can start from
+  // to avoid unnecessary file scanning / walking
+  segments.forEach((segment) => {
+    if (segment.indexOf('*') < 0) {
+      startingSegmentOffset += `/${segment}`;
+    }
+  });
+
+  // console.log({ dependency, sub, subValue, resolvedRoot, segments });
+
+  // ideally we can use fs.glob when it comes out of experimental
+  // https://nodejs.org/docs/latest-v22.x/api/fs.html#fspromisesglobpattern-options
+  function walkDirectory(directoryUrl) {
+    const filesInDir = fs.readdirSync(directoryUrl);
+
+    filesInDir.forEach(file => {
+      const filePathUrl = new URL(`./${file}`, directoryUrl);
+      const stat = fs.statSync(filePathUrl);
+
+      if (stat.isDirectory()) {
+        // console.log('>>>>>> keep walking');
+        walkDirectory(new URL(`./${file}/`, directoryUrl));
+      } else if (matchPattern(filePathUrl.href, `${resolvedRoot}${sub.replace('./', '')}`)) {
+        // console.log('$$$$$$ we got a match!');
+        const relativePath = filePathUrl.href.replace(resolvedRoot, '');
+
+        // console.log({ startingSegmentOffset, relativePath });
+        updateImportMap(`${dependency}${startingSegmentOffset}/${file}`, `/node_modules/${dependency}/${relativePath}`);
+      }
+    });
+  }
+
+  // Start the directory search from the root
+  walkDirectory(new URL(`.${startingSegmentOffset}/`, resolvedRoot));
+}
+
+// async function walkExportPatterns(dependency, sub, subValue, resolvedRoot) {
+//   console.log('walkExportPatterns', { dependency, sub, subValue, resolvedRoot });
+//   const exportPatternFiles = [];
+//   const segments = subValue.split('/').filter((segment) => segment !== '.');
+
+//   console.log({ segments });
+
+//   segments.reduce((acc, curr, index) => {
+//     console.log({ acc, curr, index });
+//     const nextSegment = `${acc}/${segments[index + 1]}`;
+
+//     if (curr.indexOf('*') >= 0) {
+//       console.log('*** handle wildcard');
+//       if (curr === '*') {
+//         // if we are NOT at the end of the pattern, keep walking
+//         if (index < segments.length - 1) {
+//           console.log('nested wildcard, walk nextSegment');
+//           return nextSegment;
+//         } else {
+//           console.log('globbing for files, track');
+//           // else grab all these files
+//           // TODO handle filtering
+//           const dirName = acc.replace(curr, '');
+//           // console.log({ dirName });
+//           const files = fs.readdirSync(new URL(`./${dirName}/`, resolvedRoot));
+//           // console.log({ files });
+//           files.forEach((file) => {
+//             exportPatternFiles.push(`./${dirName}${file}`);
+//           });
+//         }
+//       }
+//     } else {
+//       console.log('### handle something else (file or directory)?');
+//       const stat = fs.statSync(new URL(`./${acc}`, resolvedRoot));
+//       if (stat.isDirectory()) {
+//         console.log('is directory, walk nextSegment');
+//         return nextSegment;
+//       }
+//     }
+
+//     // return `${acc}/${segments[index + 1]}`;
+//   }, segments[0]);
+//   // segments.f()
+
+//   // function walkDirectory(directory) {
+//   //   const filesInDir = fs.readdirSync(directory);
+
+//   //   filesInDir.forEach((file) => {
+//   //     const fileUrl = new URL(`.${file}`, directory);
+//   //     const stat = fs.statSync(fileUrl);
+
+//   //     console.log({ fileUrl, stat });
+
+//   //     if (stat.isDirectory()) {
+//   //       console.log('keep walking')
+//   //       // Recursively search inside directories
+//   //       // walkDirectory(filePath);
+//   //     } else {
+//   //       console.log('find a match');
+//   //     }
+//   //     // } else if (matchPattern(filePath, pattern)) {
+//   //     //   // If file matches the pattern, add to the result
+//   //     //   files.push(filePath);
+//   //     // }
+//   //   });
+//   // }
+
+//   // walkDirectory(resolvedRoot);
+
+//   // if (sub.endsWith('*')) {
+//   //   const dir = new URL(sub.replace('*', ''), resolvedRoot);
+//   //   const files = await fs.readdir(dir);
+
+//   //   files
+//   //     .filter((file) => {
+//   //       let shouldNotFilter = true;
+
+//   //       // TODO do we actually need this filter?
+//   //       extensionFilters.forEach((extFilter) => {
+//   //         if (file.endsWith(extFilter)) {
+//   //           shouldNotFilter = false;
+//   //         }
+//   //       });
+
+//   //       return shouldNotFilter;
+//   //     })
+//   //     .forEach((file) => {
+//   //       updateImportMap(`${dependency}/${sub.replace('/*', '')}/${file}`, `/node_modules/${dependency}/${sub.replace('/*', '')}/${file}`);
+//   //     });
+//   // } else {
+//   //   // TODO
+//   //   // "./feature/*": "./feature/*.js",
+//   // }
+
+//   console.log({ exportPatternFiles })
+
+//   // ./dist/themes/*
+//   // {
+//   //   exportPatternFiles: [
+//   //     './dist/themes/dark.css',
+//   //     './dist/themes/dark.styles.d.ts',
+//   //     './dist/themes/dark.styles.js',
+//   //     './dist/themes/light.css',
+//   //     './dist/themes/light.styles.d.ts',
+//   //     './dist/themes/light.styles.js'
+//   //   ]
+//   // }
+//   // expand sub to all subValue
+//   exportPatternFiles.forEach((patternFile) => {
+//     // if(patternFile.indexOf)
+//     if (sub.endsWith('*')) {
+//       updateImportMap(`${dependency}/${patternFile}`, `/node_modules/${dependency}/${patternFile}`);
+//     }
+//   })
+//   // TODO apply exportPatternFiles to importMap
+// }
+
+// https://nodejs.org/api/packages.html#conditional-exports
 async function walkPackageForExports(dependency, packageJson, resolvedRoot) {
   const { exports, module, main } = packageJson;
 
+  // favor exports over main / module
   if (exports) {
-    for (const exp in exports) {
-      if (typeof exports[exp] === 'object') {
-        if (exports[exp].import) {
-          if (typeof exports[exp].import === 'object') {
-            if (exp === '.') {
-              updateImportMap(dependency, `/node_modules/${dependency}/${exports[exp].import.default ?? exports[exp].import }`);
+    for (const sub in exports) {
+      /* test for conditional subpath exports
+       * 1. import
+       * 2. default
+       */
+      if (typeof exports[sub] === 'object') {
+        if (exports[sub].import) {
+          // nested conditions
+          if (typeof exports[sub].import === 'object') {
+            if (sub === '.') {
+              updateImportMap(dependency, `/node_modules/${dependency}/${exports[sub].import.default ?? exports[sub].import }`);
             } else {
-              updateImportMap(`${dependency}/${exp}`, `/node_modules/${dependency}/${exports[exp].import.default ?? exports[exp].import}`);
+              updateImportMap(`${dependency}/${sub}`, `/node_modules/${dependency}/${exports[sub].import.default ?? exports[sub].import}`);
             }
           } else {
             // https://unpkg.com/browse/redux@5.0.1/package.json
-            updateImportMap(dependency, `/node_modules/${dependency}/${exports[exp].import }`);
+            updateImportMap(dependency, `/node_modules/${dependency}/${exports[sub].import }`);
           }
-        } else if (exports[exp].default) {
-          if (exp === '.') {
-            updateImportMap(dependency, `/node_modules/${dependency}/${exports[exp].default}`);
+        } else if (exports[sub].default) {
+          if (sub === '.') {
+            updateImportMap(dependency, `/node_modules/${dependency}/${exports[sub].default}`);
           } else {
-            updateImportMap(`${dependency}/${exp}`, `/node_modules/${dependency}/${exports[exp].default}`);
+            updateImportMap(`${dependency}/${sub}`, `/node_modules/${dependency}/${exports[sub].default}`);
           }
         } else {
           // TODO what to do here?  what else is there besides default?
         }
       } else {
-        if (exp === '.') {
-          updateImportMap(dependency, `/node_modules/${dependency}/${exports[exp]}`);
-        } else if (exp.indexOf('*') >= 0) {
-          await walkExportPatterns(dependency, exp, resolvedRoot);
+        // handle subpath exports
+        if (sub === '.') {
+          updateImportMap(dependency, `/node_modules/${dependency}/${exports[sub]}`);
+        } else if (sub.indexOf('*') >= 0) {
+          await walkExportPatterns(dependency, sub, exports[sub], resolvedRoot);
         } else {
-          updateImportMap(`${dependency}/${exp}`, `/node_modules/${dependency}/${exp}`);
+          updateImportMap(`${dependency}/${sub}`, `/node_modules/${dependency}/${sub}`);
         }
       }
     }
   } else if (module || main) {
     updateImportMap(dependency, `/node_modules/${dependency}/${module ?? main}`);
+  } else {
+    // TODO warn about no exports found
   }
 }
 
