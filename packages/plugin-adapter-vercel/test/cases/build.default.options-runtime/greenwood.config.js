67c4635aa68680469c030a583964c32420825b59
Owen Buckley <owenbuckley13@gmail.com>
2024-11-28 13:19:00 -0500

    configurable serverless runtime nodejs version

---
+++ packages/plugin-adapter-vercel/test/cases/build.default.options-runtime/greenwood.config.js
@@ -0,0 +1,9 @@
+import { greenwoodPluginAdapterVercel } from '../../../src/index.js';
+
+export default {
+  plugins: [
+    greenwoodPluginAdapterVercel({
+      runtime: 'nodejs22.x'
+    })
+  ]
+};
