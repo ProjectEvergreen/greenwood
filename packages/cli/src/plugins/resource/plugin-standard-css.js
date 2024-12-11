69486315def6916aed571290c1b81479d7ca6842
Owen Buckley <owenbuckley13@gmail.com>
2024-12-10 21:27:04 -0500

    variable renaming

--- packages/cli/src/plugins/resource/plugin-standard-css.js
+++ packages/cli/src/plugins/resource/plugin-standard-css.js
@@ -10,7 +10,7 @@ import { parse, walk } from 'css-tree';
 import { ResourceInterface } from '../../lib/resource-interface.js';
 import { hashString } from '../../lib/hashing-utils.js';
 
-function bundleCss(body, url, compilation, resolvedUrl) {
+function bundleCss(body, sourceUrl, compilation, workingUrl) {
   const { projectDirectory, outputDir, userWorkspace } = compilation.context;
   const ast = parse(body, {
     onParseError(error) {
@@ -30,18 +30,18 @@ function bundleCss(body, url, compilation, resolvedUrl) {
           if (value.indexOf('.') === 0 || value.indexOf('/node_modules') === 0) {
             const resolvedUrl = value.startsWith('/node_modules')
               ? new URL(`.${value}`, projectDirectory)
-              : new URL(value, url);
+              : new URL(value, sourceUrl);
 
             const importContents = fs.readFileSync(resolvedUrl, 'utf-8');
 
-            optimizedCss += bundleCss(importContents, url, compilation, resolvedUrl);
-          } else if (resolvedUrl) {
-            const relativeUrl = new URL(`./${value}`, resolvedUrl);
-            const importContents = fs.readFileSync(relativeUrl, 'utf-8');
+            optimizedCss += bundleCss(importContents, sourceUrl, compilation, resolvedUrl);
+          } else if (workingUrl) {
+            const resolvedUrl = new URL(`./${value}`, workingUrl);
+            const importContents = fs.readFileSync(resolvedUrl, 'utf-8');
 
-            optimizedCss += bundleCss(importContents, resolvedUrl, compilation);
+            optimizedCss += bundleCss(importContents, workingUrl, compilation);
           } else {
-            console.warn(`Unable to resolve ${value} from file => ${url}`);
+            console.warn(`Unable to resolve ${value} from file => ${sourceUrl}`);
           }
         } else {
           optimizedCss += `@import url('${value}');`;
