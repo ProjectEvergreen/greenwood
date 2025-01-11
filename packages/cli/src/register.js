db2f7c12c27b9eb6c772e82ba3a8e1aa2d5b0c37
Owen Buckley <owenbuckley13@gmail.com>
2025-01-11 14:00:21 -0500

    clean up comments

--- packages/cli/src/register.js
+++ packages/cli/src/register.js
@@ -1,5 +1,3 @@
 import { register } from 'node:module';
 
 register('./loader.js', import.meta.url);
-
-// await import('./index.js');
--- test/test-register.js
+++ test/test-register.js
@@ -1,5 +1,3 @@
 import { register } from 'node:module';
 
 register('./test-loader.js', import.meta.url);
-
-// await import('./index.js');
