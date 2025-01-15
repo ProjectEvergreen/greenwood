a9f12c255661dd235b2f8baf4232334689fc65a9
Owen Buckley <owenbuckley13@gmail.com>
2025-01-15 09:24:58 -0500

    add test case

--- packages/cli/test/cases/serve.default.api/src/pages/api/nothing.js
+++ packages/cli/test/cases/serve.default.api/src/pages/api/nothing.js
@@ -1,4 +1,8 @@
+// here to test - https://github.com/ProjectEvergreen/greenwood/pull/1384
+import * as callBoundIntrinsic from 'call-bind/callBound';
+
 export async function handler() {
+  console.log({ callBoundIntrinsic });
   return new Response(null, {
     status: 204
   });
