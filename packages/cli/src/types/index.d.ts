7ef66a9a8d7ff6f745ad79d4d56786ca15e750c3
Owen Buckley <owenbuckley13@gmail.com>
2025-02-03 14:38:47 -0500

    config and plugin type definitions

---
+++ packages/cli/src/types/index.d.ts
@@ -0,0 +1,14 @@
+import type { Config } from "./config.d.ts";
+import type { Plugin, AdapterPlugin, ContextPlugin, CopyPlugin, RendererPlugin, ResourcePlugin, RollupPlugin, ServerPlugin, SourcePlugin } from "./plugins.d.ts";
+
+export type {
+  Config,
+  AdapterPlugin,
+  ContextPlugin,
+  CopyPlugin,
+  RendererPlugin,
+  ResourcePlugin,
+  RollupPlugin,
+  ServerPlugin,
+  SourcePlugin
+};
