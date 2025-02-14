aab9b14cd319392dd4934500a2b8b1b531d80dad
Owen Buckley <owenbuckley13@gmail.com>
2025-02-14 15:22:35 -0500

    initial implementation for an AWS adapter

---
+++ packages/plugin-adapter-aws/test/cases/build.default/src/services/artists.js
@@ -0,0 +1,14 @@
+function getArtists() {
+  return [
+    {
+      name: "Analog",
+      imageUrl: "analog.png",
+    },
+    {
+      name: "Fave",
+      imageUrl: "fave.png",
+    },
+  ];
+}
+
+export { getArtists };
