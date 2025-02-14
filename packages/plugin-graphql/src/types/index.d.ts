452ea28183092f61989b90726a1c9da3b4ed0e56
Owen Buckley <owenbuckley13@gmail.com>
2025-02-14 18:50:02 -0500

    types for graphql client

--- packages/plugin-graphql/src/types/index.d.ts
+++ packages/plugin-graphql/src/types/index.d.ts
@@ -1,3 +1,22 @@
 import type { Plugin } from "@greenwood/cli/src/types/index.d.ts";
 
 export type GraphQLPlugin = () => Array<Plugin>;
+
+export type CollectionVariables = {
+  name: string;
+  orderBy?: number;
+};
+
+export type ChildrenVariables = {
+  parent?: string;
+};
+
+export type Params = {
+  query: string;
+  variables: CollectionVariables | ChildrenVariables;
+};
+
+// this could probably be better fleshed out for all query types...
+export type Client = () => {
+  query(params: Params);
+};
