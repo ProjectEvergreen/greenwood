4284f8c81384e46ddfd9527430aade8ee04f4928
Owen Buckley <owenbuckley13@gmail.com>
2025-03-30 14:26:58 -0400

    support options for markdown plugins

--- packages/cli/src/plugins/resource/plugin-standard-html.js
+++ packages/cli/src/plugins/resource/plugin-standard-html.js
@@ -64,12 +64,15 @@ class StandardHtmlResource {
       const remarkPlugins = [];
 
       for (const plugin of config.markdown.plugins) {
-        if (plugin.indexOf("rehype-") >= 0) {
-          rehypePlugins.push((await import(plugin)).default);
-        }
+        const name = typeof plugin === "string" ? plugin : plugin.name;
+        const options = plugin?.options ?? null;
+        const pluginTypeArray = name.indexOf("rehype-") >= 0 ? rehypePlugins : remarkPlugins;
+        const pluginImport = (await import(name)).default;
 
-        if (plugin.indexOf("remark-") >= 0) {
-          remarkPlugins.push((await import(plugin)).default);
+        if (options) {
+          pluginTypeArray.push([pluginImport, options]);
+        } else {
+          pluginTypeArray.push(pluginImport);
         }
       }
 
