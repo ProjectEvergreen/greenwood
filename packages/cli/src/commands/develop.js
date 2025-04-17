0478819d4460b5ebec8e7eb990fa47584f4a39a6
Owen Buckley <owenbuckley13@gmail.com>
2025-04-17 17:21:59 -0400

    remove async executor functions from develop and serve commands

--- packages/cli/src/commands/develop.js
+++ packages/cli/src/commands/develop.js
@@ -5,31 +5,26 @@ const runDevServer = async (compilation) => {
   const { port } = devServer;
   const postfixSlash = basePath === "" ? "" : "/";
 
-  // we intentionally _don't_ want this promise to resolve to keep the servers "hanging" for development
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    try {
-      (await getDevServer(compilation)).listen(port, async () => {
-        console.info(
-          `Started local development server at http://localhost:${port}${basePath}${postfixSlash}`,
-        );
+  (await getDevServer(compilation)).listen(port, async () => {
+    console.info(
+      `Started local development server at http://localhost:${port}${basePath}${postfixSlash}`,
+    );
 
-        const servers = [
-          ...compilation.config.plugins
-            .filter((plugin) => {
-              return plugin.type === "server";
-            })
-            .map((plugin) => plugin.provider(compilation)),
-        ];
+    const servers = [
+      ...compilation.config.plugins
+        .filter((plugin) => {
+          return plugin.type === "server";
+        })
+        .map((plugin) => plugin.provider(compilation)),
+    ];
 
-        for (const server of servers) {
-          server.start();
-        }
-      });
-    } catch (err) {
-      reject(err);
+    for (const server of servers) {
+      server.start();
     }
   });
+
+  // we intentionally _don't_ want this promise to resolve to keep the servers "hanging" for development
+  return new Promise(() => {});
 };
 
 export { runDevServer };
