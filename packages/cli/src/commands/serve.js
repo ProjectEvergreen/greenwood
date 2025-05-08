6c6e3fd670548151e3824ec91c68dc33468dd4e0
Owen Buckley <owenbuckley13@gmail.com>
2025-05-07 22:16:40 -0400

    enhancement/issue 1491 refactor out usages of async Promise executors (#1477)

--- packages/cli/src/commands/serve.js
+++ packages/cli/src/commands/serve.js
@@ -2,25 +2,21 @@ import { getStaticServer, getHybridServer } from "../lifecycles/serve.js";
 import { checkResourceExists } from "../lib/resource-utils.js";
 
 const runProdServer = async (compilation) => {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    try {
-      const { basePath, port } = compilation.config;
-      const postfixSlash = basePath === "" ? "" : "/";
-      const hasApisDir = await checkResourceExists(compilation.context.apisDir);
-      const hasDynamicRoutes = compilation.graph.find((page) => page.isSSR && !page.prerender);
-      const server =
-        (hasDynamicRoutes && !compilation.config.prerender) || hasApisDir
-          ? getHybridServer
-          : getStaticServer;
+  const { basePath, port } = compilation.config;
+  const postfixSlash = basePath === "" ? "" : "/";
+  const hasApisDir = await checkResourceExists(compilation.context.apisDir);
+  const hasDynamicRoutes = compilation.graph.find((page) => page.isSSR && !page.prerender);
+  const server =
+    (hasDynamicRoutes && !compilation.config.prerender) || hasApisDir
+      ? getHybridServer
+      : getStaticServer;
 
-      (await server(compilation)).listen(port, () => {
-        console.info(`Started server at http://localhost:${port}${basePath}${postfixSlash}`);
-      });
-    } catch (err) {
-      reject(err);
-    }
+  (await server(compilation)).listen(port, () => {
+    console.info(`Started server at http://localhost:${port}${basePath}${postfixSlash}`);
   });
+
+  // we intentionally _don't_ want this promise to resolve to keep the server "hanging" for production
+  return new Promise(() => {});
 };
 
 export { runProdServer };
