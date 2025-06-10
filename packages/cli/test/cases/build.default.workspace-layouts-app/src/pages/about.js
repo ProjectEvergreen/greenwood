4fa6ab17b28f8a9701fa3d02b7796799bddd6ef7
Owen Buckley <owenbuckley13@gmail.com>
2025-06-09 21:50:44 -0400

    WIP

---
+++ packages/cli/test/cases/build.default.workspace-layouts-app/src/pages/about.js
@@ -0,0 +1,19 @@
+const html = `
+<html>
+  <head>
+    <title>About Page</title>
+  </head>
+  <body>
+    <h1>About Page</h1>
+  </body>
+</html>
+`;
+
+
+export default class AboutPage extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = html;
+  }
+}
+
+export const prerender = true;
