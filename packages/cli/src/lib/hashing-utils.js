ebb4c35d044857331c9d73c2b2f0d7f1bb68ac32
Shyok Mutsuddi <shyokmutsuddi21@gmail.com>
2025-09-02 18:39:20 +0000

    fix: replaced hashing algorithm to shake256

--- packages/cli/src/lib/hashing-utils.js
+++ packages/cli/src/lib/hashing-utils.js
@@ -1,7 +1,12 @@
 import { createHash } from "node:crypto";
 
 function hashString(inputString, length = 8) {
-  return createHash("md5").update(inputString).digest("hex").slice(0, length);
+  const bytes = Math.ceil(length / 2);
+  const hash = createHash("shake256", { outputLength: bytes }).update(inputString).digest("hex");
+  if (length % 2 == 0) {
+    return hash;
+  }
+  return hash.slice(0, length);
 }
 
 const HASH_8_REGEX = "[a-zA-Z0-9]{8}";
