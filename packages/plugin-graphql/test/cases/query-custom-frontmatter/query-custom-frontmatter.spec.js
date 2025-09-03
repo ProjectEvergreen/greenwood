94348625fce55587ca35d1686486924d74e641c1
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2025-09-02 20:50:36 -0700

    debug

--- packages/plugin-graphql/test/cases/query-custom-frontmatter/query-custom-frontmatter.spec.js
+++ packages/plugin-graphql/test/cases/query-custom-frontmatter/query-custom-frontmatter.spec.js
@@ -49,7 +49,7 @@ describe("Build Greenwood With: ", function () {
     this.context = {
       publicDir: path.join(outputPath, "public"),
     };
-    runner = new Runner();
+    runner = new Runner(true);
   });
 
   describe(LABEL, function () {
