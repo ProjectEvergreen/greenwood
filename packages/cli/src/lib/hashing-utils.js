6ed78e8b903b910292f0e1ce6d30548b00b789e5
Shyok Mutsuddi <shyokmutsuddi21@gmail.com>
2025-09-05 12:20:53 +0000

    fix: replaced hashed output from hex to base64 for more security

--- packages/cli/src/lib/hashing-utils.js
+++ packages/cli/src/lib/hashing-utils.js
@@ -2,13 +2,19 @@ import { createHash } from "node:crypto";
 
 function hashString(inputString, length = 8) {
   const bytes = Math.ceil(length / 2);
-  const hash = createHash("shake256", { outputLength: bytes }).update(inputString).digest("hex");
+  const hash = createHash("shake256", { outputLength: bytes })
+    .update(inputString)
+    .digest("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "_");
+
   if (length % 2 == 0) {
     return hash;
   }
   return hash.slice(0, length);
 }
 
-const HASH_REGEX = "[a-zA-Z0-9-_=]{8}";
+const HASH_REGEX = "[a-zA-Z0-9-_]{8}";
 
 export { hashString, HASH_REGEX };
