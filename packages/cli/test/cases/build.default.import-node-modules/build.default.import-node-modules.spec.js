eb1f00dc97b22e8c45e0ef50a0f5e2146e496a78
Shyok Mutsuddi <shyokmutsuddi21@gmail.com>
2025-09-10 14:11:44 +0000

    fix: updated font-awesome regex to fix test failure

--- packages/cli/test/cases/build.default.import-node-modules/build.default.import-node-modules.spec.js
+++ packages/cli/test/cases/build.default.import-node-modules/build.default.import-node-modules.spec.js
@@ -201,14 +201,9 @@ describe("Build Greenwood With: ", function () {
         );
         const contents = fs.readFileSync(themeFile[0], "utf-8");
         const contentsRegex = new RegExp(
-          `"@font-face {font-family:'FontAwesome';src:url('/node-modules/font-awesome/fonts/fontawesome-webfont.${HASH_REGEX}.eot?v=4.7.0');`,
+          `@font-face\\s*\\{font-family:'FontAwesome';src:url\\('/node-modules/font-awesome/fonts/fontawesome-webfont\\.${HASH_REGEX}\\.eot\\?v=4\\.7\\.0'\\);`,
         );
         expect(contents).to.match(contentsRegex);
-        // expect(
-        //   contents.indexOf(
-        //     "@font-face {font-family:'FontAwesome';src:url('/node-modules/font-awesome/fonts/fontawesome-webfont",
-        //   ) > 0,
-        // ).to.equal(true);
       });
     });
   });
