762b9ec435888f1eee5279e713c5fbb599d4668c
Owen Buckley <owenbuckley13@gmail.com>
2025-09-13 10:25:19 -0400

    refactor graphql data caching implementation

--- packages/plugin-graphql/src/core/common.js
+++ packages/plugin-graphql/src/core/common.js
@@ -1,4 +1,15 @@
-import { hashString } from "@greenwood/cli/src/lib/hashing-utils.js";
+// https://gist.github.com/hyamamoto/fd435505d29ebfa3d9716fd2be8d42f0#gistcomment-2775538
+function hashString(inputString) {
+  let h = 0;
+
+  for (let i = 0; i < inputString.length; i += 1) {
+    h = (Math.imul(31, h) + inputString.charCodeAt(i)) | 0;
+  }
+
+  return Math.abs(h).toString();
+}
+
+export { hashString };
 
 function getQueryHash(query, variables = {}) {
   const queryKeys = query;
