a0872a460c60ead4e48bef8c08f6398d7f2607d0
Owen Buckley <owenbuckley13@gmail.com>
2025-11-01 23:26:55 -0400

    chore(workspace): #1315 fix feature type for commitlint (#1607)

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@
 export default {
   rules: {
     "type-case": [2, "always", "lower-case"],
-    "type-enum": [2, "always", ["feat", "enhancement", "fix", "chore", "docs", "revert"]],
+    "type-enum": [2, "always", ["feature", "enhancement", "fix", "chore", "docs", "revert"]],
     "type-empty": [2, "never"],
     "scope-case": [2, "always", "lower-case"],
     "scope-enum": [
