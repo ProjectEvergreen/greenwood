8e2253170e92133635e1ae1dbf5306f77ee645ed
Owen Buckley <owenbuckley13@gmail.com>
2025-11-01 20:33:13 -0400

    feature(plugins): #1605 add support for importing tsx

--- packages/plugin-import-jsx/src/index.js
+++ packages/plugin-import-jsx/src/index.js
@@ -3,28 +3,29 @@
  * Compile Web Components rendering with JSX using wc-compiler.
  *
  */
-import escodegen from "escodegen";
+import { generate } from "astring";
 import { parseJsx } from "wc-compiler/src/jsx-loader.js";
 
 class ImportJsxResource {
-  constructor(compilation) {
+  constructor(compilation, options) {
     this.compilation = compilation;
-    this.extensions = ["jsx"];
+    this.extensions = ["jsx", "tsx"];
     this.contentType = "text/javascript";
+    this.servePage = options.servePages ? "dynamic" : null;
   }
 
   async shouldServe(url) {
     const { pathname, protocol } = url;
     const ext = pathname.split(".").pop();
 
-    return protocol === "file:" && ext === this.extensions[0];
+    return protocol === "file:" && this.extensions.includes(ext);
   }
 
   async serve(url) {
     // refactor when WCC refactors
     // https://github.com/ProjectEvergreen/wcc/issues/116
     const tree = parseJsx(url);
-    const result = escodegen.generate(tree);
+    const result = generate(tree);
 
     return new Response(result, {
       headers: new Headers({
@@ -35,11 +36,11 @@ class ImportJsxResource {
 }
 
 /** @type {import('./types/index.d.ts').ImportJsxPlugin} */
-const greenwoodPluginImportJsx = () => [
+const greenwoodPluginImportJsx = (options = { servePages: true }) => [
   {
     type: "resource",
     name: "plugin-import-jsx:resource",
-    provider: (compilation) => new ImportJsxResource(compilation),
+    provider: (compilation) => new ImportJsxResource(compilation, options),
   },
 ];
 
