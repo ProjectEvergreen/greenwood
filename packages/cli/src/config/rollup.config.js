4bb40cbb21b8eb11c8464ffb5bd504e0c1611e84
Owen Buckley <owenbuckley13@gmail.com>
2025-12-13 14:30:04 -0500

    enhancement(cli): restore poyfill-type support in rollup config

--- packages/cli/src/config/rollup.config.js
+++ packages/cli/src/config/rollup.config.js
@@ -496,6 +496,18 @@ function greenwoodSyncImportAttributes(compilation) {
                   resource.optimizationAttr === "inline" ||
                   compilation.config.optimization === "inline";
 
+                // TODO: hmmm, seems the `polyfill=type` logic is not needed here and lower in this file?
+                if (importAttributes && importAttributes.includes(extension)) {
+                  importAttributes.forEach((attribute) => {
+                    if (attribute === extension) {
+                      bundles[bundle].code = bundles[bundle].code.replace(
+                        new RegExp(`"with{type:"${attribute}"}`, "g"),
+                        `?polyfill=type-${extension}"`,
+                      );
+                    }
+                  });
+                }
+
                 if (
                   resource.sourcePathURL.pathname ===
                     new URL(value, compilation.context.projectDirectory).pathname &&
