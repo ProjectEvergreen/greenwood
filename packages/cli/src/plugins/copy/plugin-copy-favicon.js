d1365b63b34bf66c0827c8a879e2bd1cec5721c0
Owen Buckley <owenbuckley13@gmail.com>
2026-01-30 18:36:59 -0500

    feature(cli): #1635 svg favicon copying (#1636)

--- packages/cli/src/plugins/copy/plugin-copy-favicon.js
+++ packages/cli/src/plugins/copy/plugin-copy-favicon.js
@@ -5,15 +5,22 @@ const greenwoodPluginCopyFavicon = [
     type: "copy",
     name: "plugin-copy-favicon",
     provider: async (compilation) => {
-      const fileName = "favicon.ico";
+      const fileNameIco = "favicon.ico";
+      const fileNameSvg = "favicon.svg";
       const { outputDir, userWorkspace } = compilation.context;
-      const faviconPathUrl = new URL(`./${fileName}`, userWorkspace);
+      const faviconPathIcoUrl = new URL(`./${fileNameIco}`, userWorkspace);
+      const faviconPathSvgUrl = new URL(`./${fileNameSvg}`, userWorkspace);
       const assets = [];
 
-      if (await checkResourceExists(faviconPathUrl)) {
+      if (await checkResourceExists(faviconPathIcoUrl)) {
         assets.push({
-          from: faviconPathUrl,
-          to: new URL(`./${fileName}`, outputDir),
+          from: faviconPathIcoUrl,
+          to: new URL(`./${fileNameIco}`, outputDir),
+        });
+      } else if (await checkResourceExists(faviconPathSvgUrl)) {
+        assets.push({
+          from: faviconPathSvgUrl,
+          to: new URL(`./${fileNameSvg}`, outputDir),
         });
       }
 
