dfaf34b18c9ceea0898774ab19d61b94df3a4f70
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2026-03-15 00:23:49 -0700

    fix(cli): replace copydir workaround with node cp
    better implementation of 7ba828a45eca5f2c94106ae56a166ef22ca293f8

--- packages/cli/src/lifecycles/copy.js
+++ packages/cli/src/lifecycles/copy.js
@@ -1,21 +1,5 @@
 import fs from "node:fs/promises";
-import { checkResourceExists } from "../lib/resource-utils.js";
 import { asyncForEach } from "../lib/async-utils.js";
-
-async function rreaddir(dir, allFiles = []) {
-  const files = (await fs.readdir(dir)).map((f) => new URL(`./${f}`, dir));
-
-  allFiles.push(...files);
-
-  await asyncForEach(
-    files,
-    async (f) =>
-      (await fs.stat(f)).isDirectory() &&
-      (await rreaddir(new URL(`file://${f.pathname}/`), allFiles)),
-  );
-
-  return allFiles;
-}
 
 async function copyFile(source, target, projectDirectory) {
   try {
@@ -30,38 +14,7 @@ async function copyFile(source, target, projectDirectory) {
 async function copyDirectory(fromUrl, toUrl, projectDirectory) {
   try {
     console.info(`copying directory... ${fromUrl.pathname.replace(projectDirectory.pathname, "")}`);
-    const files = await rreaddir(fromUrl);
-
-    if (files.length > 0) {
-      if (!(await checkResourceExists(toUrl))) {
-        await fs.mkdir(toUrl, {
-          recursive: true,
-        });
-      }
-
-      await asyncForEach(files, async (fileUrl) => {
-        const targetUrl = new URL(
-          `file://${fileUrl.pathname.replace(fromUrl.pathname, toUrl.pathname)}`,
-        );
-        const isDirectory = (await fs.stat(fileUrl)).isDirectory();
-
-        // eject early, we will make directories on the fly as we copy over files. since we are copying recursively and concurrently.
-        // not sure if its the most performant, but otherwise we will get errors if directories are not ready at time of file copy
-        if (isDirectory) {
-          return;
-        }
-
-        const targetUrlDir = new URL(`${targetUrl.href.split("/").slice(0, -1).join("/")}/`);
-
-        if (!(await checkResourceExists(targetUrlDir))) {
-          await fs.mkdir(targetUrlDir, {
-            recursive: true,
-          });
-        }
-
-        await copyFile(fileUrl, targetUrl, projectDirectory);
-      });
-    }
+    await fs.cp(fromUrl, toUrl, { recursive: true });
   } catch (e) {
     console.error("ERROR", e);
   }
