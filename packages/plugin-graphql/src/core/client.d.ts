e5270190250a65738157e4476be3f7b717d6b828
Owen Buckley <owenbuckley13@gmail.com>
2026-06-28 09:41:29 -0400

    fix(cli): #1622 fix get static params types (#1698)

--- packages/plugin-graphql/src/core/client.d.ts
+++ packages/plugin-graphql/src/core/client.d.ts
@@ -1,6 +1,6 @@
 declare module "@greenwood/plugin-graphql/src/core/client.js" {
   const Client: {
-    query: (params: string) => Promise;
+    query: (params: string) => Promise<Response>;
   };
 
   export default Client;
