598ec156c62e86cde8bd6a8c0cb4226703539f83
Owen Buckley <owenbuckley13@gmail.com>
2026-07-22 21:42:48 -0400

    chore(workspace): bump linux github actions to ubuntu-latest (#1555)

--- .github/workflows/chromium-lib-install.sh
+++ .github/workflows/chromium-lib-install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
 sudo apt-get update \
-  && sudo apt-get install -yq libgconf-2-4 \
   && sudo apt-get install -y wget --no-install-recommends \
   && sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
   && sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
