59bc75dadb38802dac935537145ed2a14988d2e7
jstockdi <jon.stockdill@gmail.com>
2026-08-29 10:01:48 -0400

    fix(cli): #1766 return a 404 when a develop page file is deleted while the server runs (#1773)

--- packages/cli/src/plugins/resource/plugin-standard-html.js
+++ packages/cli/src/plugins/resource/plugin-standard-html.js
@@ -53,7 +53,16 @@ class StandardHtmlResource {
     let html = "";
 
     if (isHtmlContent) {
-      body = await fs.readFile(new URL(pageHref), "utf-8");
+      try {
+        body = await fs.readFile(new URL(pageHref), "utf-8");
+      } catch (e) {
+        // in development the graph is only generated once, so a matching page may since be deleted
+        if (e.code === "ENOENT" && process.env.__GWD_COMMAND__ === "develop") {
+          return new Response(null, { status: 404 });
+        }
+
+        throw e;
+      }
 
       // purge frontmatter data from HTML files that only have a `<body>` tag
       // frontmatter outside of an <html> tag will get ignored by node-html-parser
